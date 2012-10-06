74f93e52661df6cdd0096697e65cde3af49a61f4
wolandscat <thomas.beale@oceaninformatics.com>
2012-10-06 18:29:43 +0100

    Adjustments to IP page.

--- about/ip.php
+++ about/ip.php
@@ -51,27 +51,20 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 			<tr>
 				<td class="TableMainC"><em>Specification</em></td>
 				<td class="TableMainC">Official specifications of the Foundation</td>
-				<td class="TableMainC">Copyright openEHR Foundation, licensed under Creative Commons CC-BY-ND</td>				
-			</tr>
-			<tr>
-				<td class="TableMainC"><em>Software</em></td>
-				<td class="TableMainC">Software with IP rights ascribed to the openEHR Foundation</td>
-				<td class="TableMainC">Apache 2 License</td>
+				<td class="TableMainC">Copyright openEHR Foundation, licensed under Creative Commons CC-BY-ND. <a href="../programs/specification/iplicense">Details</a></td>
 			</tr>
 			<tr>
 				<td class="TableMainC"><em>Clinical Models</em></td>
 				<td class="TableMainC">Archetypes, Templates and Terminology subsets developed by the community</td>
-				<td class="TableMainC">Creative Commons for organisational and individual use. CC-BY-(SA) The Share Alike (SA) is specifically applied to derived archatypes and templates only.</td>
+				<td class="TableMainC">Creative Commons for organisational and individual use. CC-BY-(SA) The Share Alike (SA) is specifically applied to derived archatypes and templates only. <a href="../programs/clinicalmodels/iplicense">Details</a></td>
+			</tr>
+			<tr>
+				<td class="TableMainC"><em>Software</em></td>
+				<td class="TableMainC">Software with IP rights ascribed to the openEHR Foundation</td>
+				<td class="TableMainC">Apache 2 License. <a href="../programs/software/iplicense">Details</a></td>
 			</tr>
 			</tbody>
 			</table>
-			<p></p>
-			<p>For more information about artefacts created in Programs and their licenses, click following links:</p>
-			<ul>
-				<li><a href="../programs/specification/iplicense">Specification Program licenses</a></li>
-				<li><a href="../programs/clinicalmodels/iplicense">Clinical Models Program licenses</a></li>
-				<li><a href="../programs/software/iplicense">Software Program licenses</a></li>
-			</ul>
 			
 			<h2>Principles</h2>
 			<p>Licensing of openEHR artefacts is based on following principles:</p>
@@ -81,7 +74,7 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 				<li>Protection to ensure that authoring of derivative models (archetypes, templates and terminology subsets) by one party does not in any way limit the rights of others to create those same models.</li>
 				<li>Forms or other software artefacts using the models as inputs can be protected in the same way as any other work is protected commercially.</li>
 				<li>Query languages using the models may be freely developed and protected commercially without restriction.</li>
-				<li>The IP of the logical model should be protected by the Foundation regardless of its physical expression (e.g. ADL, XML etc).</li>
+				<li>The IP of the specifications of models (e.g. openEHR Reference Model, Archetype formalism etc) is protected by the Foundation independently of physical expressions (e.g. ADL, XML etc), which are typically not definitive and may be licensed more openly.</li>
 				<li>Require Contributors to declare if there are any current or future IP or patent claims in any content being contributed.</li>
 				<li>Have formal agreement to use any measurement scales (eg Glasgow Coma, Braden etc) if required.</li>
 				<li>Have a formal agreement to use any terminology codes, value sets and reference sets where required.</li>
