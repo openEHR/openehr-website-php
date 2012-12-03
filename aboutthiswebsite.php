4ae15426612fa2ee659f9e9d3ed1d9317e0bd6d3
wolandscat <thomas.beale@oceaninformatics.com>
2012-12-03 16:34:00 +0000

    Add new text on top of Adriana's latest content.

--- aboutthiswebsite.php
+++ aboutthiswebsite.php
@@ -26,6 +26,72 @@
 	
 		<div id="TextArea" style="left:0px; width:900px; height:1300px;">
 		
+<<<<<<< HEAD
+			<h1>About this Website</h1>
+			<p>This new website aims to be more representative of the openEHR community aims and activities and particularly what people are working on out there. Don't consider it as fixed, but rather as a living thing. We are interested in feedback on any aspect of the website. In the initial development phase, visual elements and style are 'draft', and we welcome ideas for improvement.</p>
+			<p>Note that the technical approach we have taken with this site is a totally manual one - that is, it is constructed from hand-built HTML, PHP, and Java-script. This gives more freedom than the controlled environment of a CMS. Moving to a CMS is not out of the question in the future, but for now, the current approach seems more maintainable. The website code is maintained on Github, and contributors can participate by providing material via that route.</p>
+
+			
+			<h2>Providing feedback</h2>
+			<p>Please provide website design ideas/feedback <a href="http://www.openehr.org/wiki/display/oecom/2012+Website+-+feedback+page">here</a>.</p>
+			<p>Routine webmaster requests <a href="mailto:webmaster@openehr.org">here</a>.
+			
+			
+			<h2>Logo</h2>
+			<p>As openEHR is moving forward we think it is time to refresh Foundation image. Besides the new website, there is also a possibility for openEHR community to create a new openEHR logo. A few proposals have been made already.</p>		
+			<p>You can vote for openEHR logo <a href="#">here</a>. Table below shows all possible candidates. You can also send your own creation <a href="mailto:webmaster@openehr.org">here</a> and we can put it as a candidate for voting.&#42; The deadline for logo candidates is 31. December 2012. The deadline for voting for logo is 31. January 2013.</p>
+			
+			<table class="TableMain">
+			<tbody>
+			<tr>
+				<th class="TableMainT"><p>Graphic representation</p></th>
+				<th class="TableMainT"><p>Logo Background</p></th>
+				<th class="TableMainT"><p>Notes</p></th>
+			</tr>
+			<tr>
+				<td class="TableMainC"><p><img width="200px" alt="old logo" title="old logo" src="files/basic/openEHRlogo.png"></p></td>
+				<td class="TableMainC">This is currently used openEHR logo. The simple graphic, but quite distinctive colours. The new website developed in similar colour scheme.</td>
+				<td class="TableMainC">The fonts in logo are standard fonts with shadow effect.</td>
+			</tr>
+			<tr>
+				<td class="TableMainC"><p><img width="200px" alt="old logo" title="old logo" src="files/basic/candidate1.png"></p></td>
+				<td class="TableMainC">This is new logo candidate from Australia. Change in colours and fonts of openEHR and new globe graphic added.</td>
+				<td class="TableMainC"></td>
+			</tr>
+			</tbody>
+			</table>
+			<br/>
+			
+			<p>&#42; If you send us your own creation, we would appreciate if you in your design consider colour scheme of the new website (the logo position will remain the same as is now).</p>
+			
+			
+			<h2>Top banner</h2>
+			<p>You can also vote for top banner appearance <a href="#">here</a>. Table below shows all possible candidates. You can also send your own picture/graphic design (size: 200x75px in transparent PNG format) <a href="mailto:webmaster@openehr.org">here</a>. The deadline for banner candidates is 31. December 2012. The deadline for voting for banners is 31. January 2013.</p>
+			
+			<table class="TableMain">
+			<tbody>
+			<tr>
+				<th class="TableMainT"><p>Graphic representation</p></th>
+				<th class="TableMainT"><p>Notes</p></th>
+			</tr>
+			<tr>
+				<td class="TableMainC"><a href="files/basic/Banner1.PNG"><img width="200px" alt="old logo" title="old logo" src="files/basic/Banner1.PNG"></a>
+				<p>Click to enlarge</p></td>
+				<td class="TableMainC">Clear graphic: this contains only openEHR logo, motto, search and links. Right now it is set as default.</td>
+			</tr>
+			<tr>
+				<td class="TableMainC"><a href="files/basic/Banner2.PNG"><img width="200px" alt="old logo" title="old logo" src="files/basic/Banner2.PNG"></a>
+				<p>Click to enlarge</p></td>
+				<td class="TableMainC">The 'doctors' graphic: this is just an idea at this stage: an image that tells the outside world that openEHR is something to do with healthcare and computing. Doctors in white coats are not particularly representative of all healthcare workers, but we have found no obvious alternative so far, since in reality most healthcare workers look like normal people. The doctors graphic may also seem more in the style of commercial e-health vendor advertising rather than a .org. Alternatives: any visual content that gives the idea of computing in healthcare.</td>
+			</tr>
+			</tbody>
+			</table>
+			<br/>
+			
+			
+			<h2>The motto</h2>
+			<p>A number of ideas have been tried here. This is meant to be a short sharp expression of what the openEHR Foundation and its community are about. Key ideas to include seems to be: archetypes/content models; flexibility/future-proofing/adaptibility of systems. Clearly no slogan can capture everything about a large effort like openEHR, so there will never be a perfect one. On the other hand being memorable is useful. Suggestions are welcome <a href="mailto:webmaster@openehr.org">here</a>.</p>
+=======
 			<h1>About this Website</h1>
 			<p>This new website aims to be more representative of the openEHR community aims and acitivities. Don't consider it as fixed, but rather as a living thing. We are interested in feedback on any aspect of the website. In the initial development phase, visual elements and style are 'draft', and we are interested in suggestions for improvement.</p>
 
