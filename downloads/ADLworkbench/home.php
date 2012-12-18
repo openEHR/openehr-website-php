e97eeda045edbfab38f2f0eb19ffd4ae4c5ec525
wolandscat <thomas.beale@oceaninformatics.com>
2012-12-18 15:44:15 +0000

    Correct links in AWB and AE download pages.

--- downloads/ADLworkbench/home.php
+++ downloads/ADLworkbench/home.php
@@ -67,25 +67,25 @@ $pageOn = basename($_SERVER['PHP_SELF']);
 			<tr>
 				<td class="TableMainC"> Windows</td>
 				<td class="TableMainC"> beta 8</td>
-				<td class="TableMainC"> <a href="http://www.openehr.org/svn/oe_distrib/windows/tools/ADLWorkbenchInstall.exe">.exe installer</a></td>
+				<td class="TableMainC"> <a href="https://github.com/openEHR/downloads/blob/master/windows/adl_workbench/ADLWorkbenchInstall.exe">.exe installer</a></td>
 				<td class="TableMainC"><br/> </td>
 			</tr>
 			<tr>
 				<td class="TableMainC"> Mac OS X 10.7 (Lion)<br/></td>
 				<td class="TableMainC"> beta 8</td>
-				<td class="TableMainC"> <a href="http://www.openehr.org/svn/oe_distrib/mac_osx/tools/ADL Workbench for Lion i386.dmg">.dmg installer</a></td>
+				<td class="TableMainC"> <a href="https://github.com/openEHR/downloads/blob/master/mac_osx_10.7/adl_workbench/ADL%20Workbench%20for%20Lion%20i386.dmg">.dmg installer</a></td>
 					<td class="TableMainC"> <a href="installationnotes#mac">here</a> <br/></td>
             </tr>
             <tr>
 					<td class="TableMainC"> Mac OS X 10.8 (Mountain Lion)<br/></td>
 					<td class="TableMainC"> beta 8</td>
-					<td class="TableMainC"> <a href="http://www.openehr.org/svn/oe_distrib/mac_osx/tools/ADL Workbench for Mountain Lion i386.dmg">.dmg installer</a></td>
+					<td class="TableMainC"> <a href="https://github.com/openEHR/downloads/blob/master/mac_osx_10.8/adl_workbench/ADL%20Workbench%20for%20Mountain%20Lion%20i386.dmg">.dmg installer</a></td>
 				<td class="TableMainC"> <a href="installationnotes#mac">here</a> <br/></td>
 			</tr>
 			<tr>
 				<td class="TableMainC">Linux (32-bit Intel)<br/></td>
 				<td class="TableMainC">beta 8</td>
-				<td class="TableMainC"> <a href="http://www.openehr.org/svn/oe_distrib/linux/tools/adl_workbench-linux.tar.bz2">.tar bz2 archive</a></td>
+				<td class="TableMainC"> <a href="https://github.com/openEHR/downloads/blob/master/linux/adl_workbench/adl_workbench-linux.tar.bz2">.tar bz2 archive</a></td>
 				<td class="TableMainC"> <a href="installationnotes#linux">here</a></td>
 			</tr>
 			</tbody>
--- downloads/archetypeeditor/home.php
+++ downloads/archetypeeditor/home.php
@@ -1,1 +1,107 @@
-<?php$pageOn = basename($_SERVER['PHP_SELF']);?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html><head>	<title>openEHR - Archetype Editor</title>	<link rel="bookmark" href="http://www.openehr.org/downloads/archetypeeditor/home" title="openEHR - Archetype Editor">	<?php include '../../panel/headpanel.php' ?>	</head><body><div id="MainFrame">		<div id="TopPanel">		<?php include '../../panel/toppanel.php' ?>	</div>		<div id="TopMenu" style="text-align:left">			<div class="nav">			<?php include '../../menu/topmenu.php' ?>		</div>			</div> 	<div id="MainArea">				<div id="SideMenu">			<?php include 'menu/editormenu.php' ?>		</div>		<div id="TextArea">					<h1>Archetype Editor Home</h1>			<br/>			<p>The Archetype Editor is a tool to support the authoring of openEHR clinical and administrative archetypes.</p>			<p>The editor is Unicode-enabled. It can work with archetypes in any language. The editor itself has been translated			into several languages, including Danish, English, Farsi, German, Japanese, Spanish, Swedish and Turkish.			If you would like to include another language, please let us know.</p>									<h2>Status - Current beta release 2. May 2011</h2>			<p>Archetype Editor 2.2 has many fixes to the handling of XML archetypes.			There are many improvements to constraint bindings and term bindings.			See the <a href="releasenotes">Release notes</a> for a complete list of the changes.</p>							<h2>Download</h2>			<p><a href="http://www.openehr.org/svn/oe_distrib/windows/tools/ArchetypeEditorInstall.exe">Download the installer</a>.<br/>			Archetype Editor is available for Windows only. .NET 2.0 is required.</p>						<h3>Sources</h3>			<p>The source code can be accessed from the <a href="http://www.openehr.org/projects/dotnet.html">openEHR project page</a>.</p>						<h3>Archetypes</h3>			<p>You need archetypes. Where you can get them:</p>			<ul>				<li>You can write them yourself, or you may already have some in your organisation </li>				<li>The openEHR archetypes are available from the <a href="http://www.openehr.org/knowledge">                Clinical Knowledge Manager</a>(CKM). Obtain a login in order to download all the archetypes.                This repository has over 250 archetypes, in various stages of development by a large number of international experts, and includes clinical and administrative archetypes.				</li>			</ul>			<p>Archetypes can be put anywhere on your file system.</p>									<h2>Using the Archetype Editor</h2>			<p>One convenient way for clinical modellers with more of a technical bent to use the Archetype Editor			is in conjunction with the ADL Workbench (AWB). There are two reasons you might want to do this:			firstly the AWB provides a tool-based view of all archetypes, rather than the user having to rely on the file system explorer;			secondly, the AWB provides ADL 1.5 validation of all archetypes.			See the <a href="http://www.openehr.org/new-ws/site/downloads/ADLworkbench/home">ADL Workbench help pages</a> for how to configure this.</p>									<h2>Support</h2>			<p>The best way to get help with the Archetype Editor and/or creating, viewing archetypes,			is to post questions on the <a href="http://lists.openehr.org/mailman/listinfo/openehr-clinical_lists.openehr.org">openEHR Clinical Mailing list</a>.</p>			<h2>Reporting Issues</h2>			<p>Please report issues on the openEHR <a href="http://www.openehr.org/issues/browse/AEPR">Archetype Editor problem report tracker (Jira)</a>.			It really helps us to help you if you report a problem! Please try to note down the steps taken to reproduce the problem, which will greatly help us in fixing it.</p>        		</div>	</div>	<div id="BottomMenu">		<?php include '../../menu/bottommenu.php' ?>	</div>	<div id="BottomPanel">		<?php include '../../panel/bottompanel.php' ?>	</div></div><script type="text/javascript">var dropdown=new MENU.dropdown.init("dropdown", {id:'menu', active:'menuhover'});</script></body></html>
+<?php $pageOn = basename($_SERVER['PHP_SELF']); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+
+	<title>openEHR - Archetype Editor</title>
+	<link rel="bookmark" href="http://www.openehr.org/downloads/archetypeeditor/home" title="openEHR - Archetype Editor">
+	<?php include '../../panel/headpanel.php' ?>
+	
+</head>
+
+
+<body>
+
+<div id="MainFrame">
+	
+	<div id="TopPanel">
+		<?php include '../../panel/toppanel.php' ?>
+	</div>
+	
+	<div id="TopMenu" style="text-align:left">
+	
+		<div class="nav">
+			<?php include '../../menu/topmenu.php' ?>
+		</div>
+		
+	</div> 
+
+	<div id="MainArea">
+		
+		<div id="SideMenu">
+			<?php include 'menu/editormenu.php' ?>
+		</div>
+
+		<div id="TextArea">
+		
+			<h1>Archetype Editor Home</h1>
+			<br/>
+			<p>The Archetype Editor is a tool to support the authoring of openEHR clinical and administrative archetypes.</p>
+			<p>The editor is Unicode-enabled. It can work with archetypes in any language. The editor itself has been translated
+			into several languages, including Danish, English, Farsi, German, Japanese, Spanish, Swedish and Turkish.
+			If you would like to include another language, please let us know.</p>
+			
+			
+			<h2>Status - Current beta release 2. May 2011</h2>
+			<p>Archetype Editor 2.2 has many fixes to the handling of XML archetypes.
+			There are many improvements to constraint bindings and term bindings.
+			See the <a href="releasenotes">Release notes</a> for a complete list of the changes.</p>
+		
+		
+			<h2>Download</h2>
+			<p><a href="https://github.com/openEHR/downloads/blob/master/windows/archetype_editor/ArchetypeEditorInstall.exe">Download the installer</a>.<br/>
+			Archetype Editor is available for Windows only. .NET 2.0 is required.</p>
+			
+			<h3>Sources</h3>
+			<p>The source code can be accessed from the <a href="http://www.openehr.org/projects/dotnet.html">openEHR project page</a>.</p>
+			
+			<h3>Archetypes</h3>
+			<p>You need archetypes. Where you can get them:</p>
+			<ul>
+				<li>You can write them yourself, or you may already have some in your organisation </li>
+				<li>The openEHR archetypes are available from the <a href="http://www.openehr.org/knowledge">
+                Clinical Knowledge Manager</a>(CKM). Obtain a login in order to download all the archetypes.
+                This repository has over 250 archetypes, in various stages of development by a large number of international experts, and includes clinical and administrative archetypes.
+				</li>
+			</ul>
+			<p>Archetypes can be put anywhere on your file system.</p>
+			
+			
+			<h2>Using the Archetype Editor</h2>
+			<p>One convenient way for clinical modellers with more of a technical bent to use the Archetype Editor
+			is in conjunction with the ADL Workbench (AWB). There are two reasons you might want to do this:
+			firstly the AWB provides a tool-based view of all archetypes, rather than the user having to rely on the file system explorer;
+			secondly, the AWB provides ADL 1.5 validation of all archetypes.
+			See the <a href="/downloads/ADLworkbench/home">ADL Workbench help pages</a> for how to configure this.</p>
+			
+			
+			<h2>Support</h2>
+			<p>The best way to get help with the Archetype Editor and/or creating, viewing archetypes,
+			is to post questions on the <a href="http://lists.openehr.org/mailman/listinfo/openehr-clinical_lists.openehr.org">openEHR Clinical Mailing list</a>.</p>
+
+
+			<h2>Reporting Issues</h2>
+			<p>Please report issues on the openEHR <a href="http://www.openehr.org/issues/browse/AEPR">Archetype Editor problem report tracker (Jira)</a>.
+			It really helps us to help you if you report a problem! Please try to note down the steps taken to reproduce the problem, which will greatly help us in fixing it.</p>
+        
+		</div>
+
+	</div>
+
+	<div id="BottomMenu">
+		<?php include '../../menu/bottommenu.php' ?>
+	</div>
+
+	<div id="BottomPanel">
+		<?php include '../../panel/bottompanel.php' ?>
+	</div>
+
+</div>
+
+<script type="text/javascript">
+var dropdown=new MENU.dropdown.init("dropdown", {id:'menu', active:'menuhover'});
+</script>
+
+</body>
+</html>
