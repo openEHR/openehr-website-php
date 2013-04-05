a7c6b4fa128e0afcd99a6716c86a0021c7640d04
wolandscat <thomas.beale@oceaninformatics.com>
2013-04-05 15:10:29 +0100

    Add a default out of service page.

---
+++ using_the_site/outofservice.php
@@ -0,0 +1,61 @@
+<?php
+$PageName = 'Out of Service';
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+<head>
+
+	<?php include '../panel/headpanel.php' ?>
+	
+</head>
+
+<body>
+
+<div id="MainFrame">
+	
+	<div id="TopPanel">
+		<?php include '../panel/toppanel.php' ?>
+	</div>
+	
+	<div id="TopMenu">
+	
+		<div class="nav">
+			<?php include '../menu/topmenu.php' ?>
+		</div>
+		
+	</div> 
+
+	<div id="MainArea" style="height:450px;">
+	
+		<div id="SideMenu">
+		<?php include 'menu.php' ?>
+		</div>
+		
+		<div id="TextArea">
+			
+			<h1><?php echo "$PageName";?></h1>
+			<br/>
+			<p>These pages down for maintenance.</p>
+			
+		</div>
+			
+	</div>
+
+	<div id="BottomMenu">
+	<?php include '../menu/bottommenu.php' ?>
+	</div>
+
+	<div id="BottomPanel">
+	<?php include '../panel/bottompanel.php' ?>
+	</div>
+
+</div>
+	
+<?php include '../panel/scriptpanel.php' ?>
+
+</body>
+
+</html>
