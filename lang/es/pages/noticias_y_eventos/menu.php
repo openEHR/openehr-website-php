7b1fe5d781054e0f9748e04bf8dc6969b7a1c345
adrianadanilakova <adriana.danilakova@gmail.com>
2013-12-11 10:56:29 +0100

    Spanish language templates and front page

---
+++ lang/es/pages/noticias_y_eventos/menu.php
@@ -0,0 +1,9 @@
+﻿<span>Noticias y Eventos</span>
+			<ul>
+				<li><a href="/news_events/industry_news" <?php if($PageName == 'Industry News'){?> class="active"<?php }?>>&nbsp; Noticias de la Industria</a></li>
+				<li><a href="/news_events/community_news" <?php if($PageName == 'Community News'){?> class="active"<?php }?>>&nbsp; Noticias de la Comunidad</a></li>
+				<li><a href="/news_events/foundation_news" <?php if($PageName == 'Foundation News'){?> class="active"<?php }?>>&nbsp; Noticias de la Fundación</a></li>
+				<li><a href="/news_events/events" <?php if($PageName == 'Events'){?> class="active"<?php }?>>&nbsp; Eventos</a></li>
+				<li><a href="/news_events/releases" <?php if($PageName == 'Releases'){?> class="active"<?php }?>>&nbsp; Liberaciones</a></li>
+				<li><a href="/news_events/clinical_model_news" <?php if($PageName == 'CKM Activity'){?> class="active"<?php }?>>&nbsp; Actividad del CKM</a></li>
+			</ul>
---
+++ lang/es/templates/_header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+	
+	<title>openEHR - <?php echo "$PageName";?></title>
+	<meta charset="utf-8">	
+	<meta name="description" content="openEHR provides open source specifications and reference implementations of future proof EHR systems">
+	<meta name="keywords" content="archetypes, openEHR, EHR, electronic health record, electronic medical record, ADL, reference model, clinical models, healthcare, health informatics, medical informatics, EHR standards, ISO 18308, CEN 13606, health care software, open source software, knowledge modelling, patient-centric">
+	<meta name="author" content="Sam Heard, Thomas Beale">
+	<meta name="designer" content="Adriana Danilakova">
+	<meta name="rating" content="General">
+	<meta name="robots" content="All">
+	<meta name="revisit-after" content="15 days">
+	<meta name="viewport" content="1024">
+
+	<link rel="stylesheet" href="/styles/basic.css"  type="text/css">
+	<link rel="stylesheet" href="/styles/pages.css"  type="text/css"> 
+	<link rel="stylesheet" href="/styles/menu.css" type="text/css"> 
+	
+	<link rel="shortcut icon" href="/gui/favicon.ico">
+	<link rel="home" href="http://www.openehr.org">
+	<link rel="index" href="/sitemap">
+
+	<script type="text/javascript" src="/menu/dropdown.js"></script> 
+	
+	<?php
+	$pageOn = basename($_SERVER['PHP_SELF']);
+	?>
+	
+</head>
+
+<body>
+
+<div id="MainFrame">
+	
+	<div id="TopPanel">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/panel/toppanel.php' ?>
+	</div>
+	
+	<div id="TopMenu">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/menu/topmenu.php' ?>
+	</div> 
+
+	<div id="MainArea">
+		
+		<div id="SideMenu">
+			<?php include 'menu.php' ?>
+		</div>
+		
