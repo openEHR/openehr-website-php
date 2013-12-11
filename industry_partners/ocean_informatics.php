61e75d2228d3f36112e23fb2e27bcb7e8e1681a5
adrianadanilakova <adriana.danilakova@gmail.com>
2013-12-11 11:36:26 +0100

    New information about industry partner

--- industry_partners/ocean_informatics.php
+++ industry_partners/ocean_informatics.php
@@ -20,11 +20,26 @@ require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header.php');
 			<p><em>Trading since:</em> 1998</p>
 			<p><a href="http://oceaninformatics.com/" target="_blank">Ocean Informatics website</a></p>
 			
+			
+			<h2>Services</h2>
+			<p><a href="http://oceaninformatics.com/services/" target="_blank">E-health consulting</a></p>
+			<p><a href="http://oceaninformatics.com/services/clinical_modelling" target="_blank">Clinical Modelling</a></p>
+			<p><a href="http://oceaninformatics.com/services/training" target="_blank">Training</a></p>
+			
 			<h2>Solutions</h2>
-			<p>xxx</p>
+			<p><a href="http://oceaninformatics.com/solutions/knowledge_management" target="_blank">Clinical Knowledge Manager (CKM)</a> - web-based collaborative model management</p>
+			<p><a href="http://oceaninformatics.com/platform/services_platform" target="_blank">OceanEHR openEHR-based services platform</a>, including Clinical Data Repository, Clinical Data Exchange</p>
+			<p><a href="http://oceaninformatics.com/solutions/clinical_product_suite" target="_blank">Multiprac Clinical applications </a>- including Infection Control and Care Planning</p>
+			<p><a href="http://oceaninformatics.com/solutions/EHR_CDR" target="_blank">Multiprac Shared EHR</a> - shared EHR for regional and community use</p>
 			
 			<h2>Customers</h2>
-			<p>xxx</p>
+			<p><a href="http://www.health.qld.gov.au/" target="_blank">Queensland Health</a></p>
+			<p><a href="http://www.health.nt.gov.au/" target="_blank">Northern Territory Health</a></p>
+			<p><a href="http://www.wentwest.com/public/" target="_blank">WentWest Medicare Local, Sydney</a></p>
+			<p><a href="http://www.leedsth.nhs.uk/home/" target="_blank">Leeds Teaching Hospital Trust</a></p>
+			<p><a href="http://www.nehta.gov.au/" target="_blank">National e-Health Transition Authority (Nehta), Australia</a></p>
+			<p><a href="http://www.mz.gov.si/en/" target="_blank">Ministry of Health, Slovenia</a></p>
+			<p><a href="http://www.helse-bergen.no/english/Sider/side.aspx" target="_blank">Helse Bergen, Norway</a></p>
 		
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
