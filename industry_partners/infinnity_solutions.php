a1a028e3ab16cf3b86d5aa36187796b74cdeea85
adrianadanilakova <adriana.danilakova@gmail.com>
2013-12-16 12:52:32 +0100

    Industry partner content added

--- industry_partners/infinnity_solutions.php
+++ industry_partners/infinnity_solutions.php
@@ -15,16 +15,20 @@ require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header.php');
 			<a href="http://www.infinnity.ru/" target="_blank"><img src="/files/industry_partners/infinnity_logo.png" width="100px" style="padding:0px 20px 0px 0px; float:left;"></a>
 			
 			<p><em>Headquarters:</em> Russia</p>
-			<p><em>Branches:</em> ...</p>
-			<p><em>Size:</em> ...</p>
-			<p><em>Trading since:</em> ...</p>
+			<p><em>Branches:</em> United Kingdom</p>
+			<p><em>Size:</em> 50+</p>
+			<p><em>Trading since:</em> 2006</p>
 			<p><a href="http://www.infinnity.ru/" target="_blank">Infinnity Solutions website</a></p>
 			
 			<h2>Solutions</h2>
-			<p>xxx</p>
+			<p>Developing and supplying e-health solutions to a wide range of Healthcare organisations - from single clinics/GP practices to large federal healthcare providers.</p>
+			
+			<h2>Solutions</h2>
+			<p>InfinniPlatform is a complete toolbox for developing e-health systems. It includes EHR Data Vault, EHR Integration Server, Server Configuration and Administration Tools, Business Logic Builder/Customisation Tools, clinical applications WYSWYG UI Designer for Desktop/iOS/Android and Reporting Services.</p>
 			
 			<h2>Customers</h2>
-			<p>xxx</p>
+			<p>Regional healthcare providers: Chelyabinsk, Khanty-Mansiysk and Moscow. Over 5.5 mln records are managed by InfinniPlatform. Over 2000 daily concurrent users.</p>
+			<p>Local healthcare providers: 6+ clinics and hospitals with nearly 1000 users and 1 mln EHR in use</p>
 		
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
