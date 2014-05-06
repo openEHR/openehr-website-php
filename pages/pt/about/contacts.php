fb60a93d0158b1a36369609048895ba1f4932463
jussararotzsch <jussara.macedo@gmail.com>
2014-05-06 11:28:29 -0300

    Update contacts.php

--- pages/pt/about/contacts.php
+++ pages/pt/about/contacts.php
@@ -12,9 +12,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/pt/_header.php');
 			
 			<h1><?php echo "$PageName";?></h1>
 			
-			<p>To contact webmaster please click <a href="mailto:webmaster@openehr.org">here</a>.</p>
-			<p>To contact a board member please click <a href="mailto:foundationoffice@openehr.org">here</a>.</p>			
-			<p>To contact the foundation office please click <a href="mailto:foundationoffice@openehr.org">here</a>.</p>
+			<p>Para contactar o  webmaster clique aqui <a href="mailto:webmaster@openehr.org">aqui</a>.</p>
+			<p>Para contactar um membro da diretoria clique aqui<a href="mailto:foundationoffice@openehr.org">aqui</a>.</p>			
+		<p>Para contactar a secretaria  da fundação clique aqui<a href="mailto:foundationoffice@openehr.org">aqui</a>.</p>
 			
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
