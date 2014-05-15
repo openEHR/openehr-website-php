3665487823ae89e7d32d4f563d555ca00e4b425d
jussararotzsch <jussara.macedo@gmail.com>
2014-05-15 16:46:24 -0300

    Update index.php

--- pages/pt/programs/clinicalmodels/index.php
+++ pages/pt/programs/clinicalmodels/index.php
@@ -12,44 +12,110 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/pt/_header.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			
-			<h2>Introduction</h2>
-			<p>The Health Information Models space is for discussions about formalised models of health domain content. In the openEHR Framework there are a number of innovations that make it relatively straightforward to specify and share clinical content. The foundations are the clinical models, consisting of archetypes and templates. These models require terminology and finally the models need to support automated clinical process, offering the clinician users decision support and suggesting quality care options in the form of computerised clinical pathways.</p>
+			<h2>Introdução</h2>
+			<p> O espaço de Modelos de Informação em Saúde se destina a discussões sobre modelos formalizados do domínio saúde> Na Framework openEHR existem um número de inovações que permitem especificar e compartilhar conteúdo clínico de forma linear. As bases para isso são os modelos clínicos, que consistem de arquétipos e templates.  Esses mpdelos requerem o uso de termninologias e finalmente esses modelos precisam apoiar o processo clínico automatizado, oferecer suporte à decisão clínica e sugerir opções para cuidados de boa qualidade na forma de linhas de cuidado computadorizadas.</p>
 			
 			
-			<h2>Clinical Models</h2>
-			<p>The openEHR community is now formally reviewing a set of archetypes for clinical use in an international setting. This is available via a web-based controlled <a href="http://www.openehr.org/wiki/display/healthmod/Clinical+Knowledge+Manager" target="_blank">authoring environment</a>. The evaluation is proceeding under the guidance of Dr Heather Leslie and the Archetype Editorial Group.</p>
+			<		<h2>Modelos Clínicos</h2>
+			<p>A comunidade  openEHR está revisando formalmente um conjunto de arquétipos para uso clínico em um cenário internacional. Esta revisão está 
+
+disponível via <a href="http://www.openehr.org/wiki/display/healthmod/Clinical+Knowledge+Manager" target="_blank">ambiente de autoração</a> controlado baseado em Web. 
+
+A avaliação está em curso sob a supervisão da Dra. Heather Leslie e do Grupo Editorial de Arquétipos.</p>
 			
-			<h3>Background</h3>
-			<p>Models of clinical information exist in every computer system that is used in health care. While doctors, nurses and other health professionals share many clinical concepts and can communicate very effectively about these concepts, computers have not had a standard means of representing clinical information. openEHR offers this capability and in doing so provides a platform for health care computing. If we do not have such a platform then the benefits of computing in health care will be very slow coming and require absolutely massive investment.</p>
+			<h3>Pano de Fundo (Background)</h3>
+			<p>Modelos de informações clínicas existem em todos os sistemas computacionais utilizados em cuidados em saúde. Enquanto médicos, enfermeiros e 
+
+outros profissionais de saúde compartilham diversos conceitos clínicos e podem comunicar de forma efetiva sobre estes conceitos, os computadores não têm um padrão 
+
+definido de significado para representar informações clínicas. openEHR oferece esta capacidade e ao fazer isso, provê uma plataforma para o processamento de cuidados 
+
+em saúde. Se não tivermos esta plataforma, então os benefícios do processamento de dados em cuidados em saúde serão muito vagarosos e necessitarão de altíssimo volume 
+
+de investimento.</p>
 			
-			<h4>Archetypes</h4>
-			<p>Archetypes are the fundamental shareable specifications of clinical information we need to provide quality health care, and have been formally accepted as a European standard in 2007 (CEN 13606 Part II) and now being considered by ISO. Each archetype represents a whole, discrete specification which is as inclusive as possible, always in terms of the openEHR reference model. The reference model itself guarantees that the key attributes for information in health records (such as who, when and where) are already taken care of and do not need to be addressed in each archetype. Further, the reference model has 5 different entry classes which provide the attributes and structures required for all the different sorts of information stored in a health record.</p>
-			<p>openEHR archetypes offer great advantage over HL7 templates as complex data can be specified in a manner that is understandable to health professionals and IT staff. It is a very efficient way to manage the specification of information we want to share between health care systems.</p>
+			<h4>Arquétipos</h4>
+			<p>Arquétipos são especificações fundamentais de informações clínicas compartilháveis necessárias ao provimento de cuidado de qualidade em 
+
+saúde, e tem sido formalmente aceitos como padrão Europeu em 2007 (CEN 13606 Part II) e agora são considerados pela ISO. Cada arquétipo representa uma especificação 
+
+completa, discreta e o mais inclusiva possível, sempre em termos do modelo de referência openEHR. O modelo de referência em si garante que os atributos chave para 
+
+registros de informação em saúde (como quem, quando e onde) são cuidados e não precisam ser tratados para cada arquétipo. Posteriormente, o modelo de referência teve 5 
+
+diferente classes de entrada que proviam atributos e estruturas necessárias a toda e qualquer informação armazenada em um registro de saúde.</p>
+			<p>Arquétipos openEHR oferecem uma grande vantagem em relação aos modelos (templates) HL7 uma vez que dados complexos podem ser especificados 
+
+de forma que seja compreensível pelos profissionais de saúde e equipe de TI. É uma forma muito eficiente de gerenciar as especificações de informações que desejamos 
+
+compartilha entre os sistemas de saúde.</p>
 			
-			<h4>Templates</h4>
-			<p>Templates are a further means of building clinical models; these are composed of one or more archetypes and add further constraints required for the use of those archetypes in a particular setting. Thus, archetypes for blood pressure, weight and blood sugar may be used when recording an annual review of a diabetic person or in an antenatal visit by a pregnant woman. That is to say, templates will be created that are specific to 'diabetic review' and 'antenatal visit'.</p>
+			<h4>Modelos (Templates)</h4>
+			<p>Modelos (Templates) são um significado mais avançado na construção do modelos clínicos; estes são compostos de um ou mais arquétipos onde 
+
+acrescentamos restrições necessárias ao uso destes arquétipos em um cenário particular. Assim, arquétipos para pressão sanguínea, peso, glicose podem ser usadas para 
+
+registrar o acompanhamento anual de uma pessoal diabética ou em uma consulta de pré-natal de uma mulher grávida. Isto é, os modelos serão criados  de forma específica 
+
+para ‘acompanhamento de diabetes’ e ‘consulta pré-natal’.</p>
 			
 			
-			<h2>Terminology</h2>
-			<p>Terminology is as old as computers in health care. Initially used to minimise disk space, the use of terminologies has grown to classify health care offered and more recently, through use of SNOMED-CT (<a href="http://www.ihtsdo.org/" target="_blank">IHTSDO</a>) to support automated clinical process. Archetypes, and openEHR for that matter, are designed to work with terminology.</p>
+			<h2>Terminologia</h2>
+			<p>Terminologia é tão antiga quanto os computadores em saúde. Inicialmente usada para minimizar espaço em disco, o uso de terminologias cresceu 
+
+a ponto de classificar o cuidado em saúde ofertado e, mais recentemente, por meio do SNOME-CT (<a href="http://www.ihtsdo.org/" target="_blank">IHTSDO</a>) para apoiar 
+
+processos clínicos automatizados. Arquétipos, e o openEHR nesta questão, são planejados para trabalhar com terminologia.</p>
 			
-			<h4>Archetypes and Terminolofy</h4>
-			<p>Every term in an archetype can be 'bound' to a terminology to better understand the authors intent. Further, archetypes allow expression of subsets to determine which terms are appropriate values at a given data point. This is called a 'constraint definition' and is really a placeholder for a valueset that is offered to the user within the application.</p>
+			<h4>Arquétipos e Terminologia</h4>
+			<p>Cada termo em um arquétipo pode ser unificado a uma terminologia para melhor compreensão da intenção do autor. Além disso, os arquétipos 
+
+permitem a expressão de subconjuntos para determinar os conjuntos de termos de valores mais adequados em um  ponto de dados específico. Isto é conhecido como 
+
+‘definição de restrição’ e  é um espaço reservado  para um conjunto de valores que é oferecido ao usuário dentro da aplicação.</p>
 			
-			<h4>Templates and Terminology</h4>
-			<p>Terminologies used in Healthcare include <a href="http://www.who.int/classifications/icd/en/" target="_blank">ICD-10</a> (and its predecessor ICD-9), <a href="http://www.ihtsdo.org/snomed-ct/" target="_blank">SNOMED-CT</a>, <a href="http://loinc.org/" target="_blank">LOINC</a> and many others.</p>
-			<p>Most terminologies use codes as identifiers or references to each individual term or concept. Codes and identifiers are generally to aid processing by computer rather than humans. They come at a price.</p>
+			<h4>Modelos (Templates) and Terminologia</h4>
+			<p>Terminologias usadas no Cuidado à Saúde incluem <a href="http://www.who.int/classifications/icd/en/" target="_blank">ICD-10</a> (e seus  
+
+predecessores ICD-9), <a href="http://www.ihtsdo.org/snomed-ct/" target="_blank">SNOMED-CT</a>, <a href="http://loinc.org/" target="_blank">LOINC</a> e muitas 
+
+outras</p>
+			<p>A maioria das terminologias utiliza códigos como identificadores ou referências para cada termo ou conceito individual. Códigos e 
+
+identificadores são utilizados geralmente para auxiliar o processamento por computador em lugar de pessoas.</p>
 			
-			<h4>Codes, EHRs and Semantic Interoperability</h4>
-			<p>One often hears that coded data are essential for semantic interoperability and decision support. Coding is the use of symbolic,  or alphanumeric identifiers to tag data items as referring to concepts or terms from an agreed vocabulary or ontology. Coding, may, in many circumstances have some value. But it also comes at a price. This <a href="http://www.openehr.org/wiki/display/healthmod/Codes%2C+EHRs+and+Semantic+Interoperability" target="_blank">article</a> looks at the balance sheet to tease out the issues facing those making recommendations for electronic health records and semantic interoperability.</p>
+			<h4>Códigos, Registros Eletrônicos em Saúde (EHRs) e Interoperabilidade Semântica</h4>
+			<p>Ouve-se com frequência que dados codificados são essenciais para a interoperabilidade semântica e suporte à decisão. Codificação é o uso de 
+
+símbolos, ou identificadores alfanuméricos para assinalar itens de dado como referência a conceitos ou termos oriundos de um vocabulário ou ontologia de consenso. 
+
+Codificação, pode, em várias circunstâncias ter o mesmo valor. Mas isto acontece por um preço. Este <a href="http://www.openehr.org/wiki/display/healthmod/Codes%2C
+
++EHRs+and+Semantic+Interoperability" target="_blank">artigo</a> faz um balanço e provoca as questões a serem enfrentadas por aqueles que fazem recomendações para 
+
+Registros Eletrônicos em Saúde e a Interoperabilidade Semântica.</p>
 			
 			
-			<h2>Clinical Knowledge Governance</h2>
-			<p>The openEHR <a href="documentation">Clinical Knowledge Manager (CKM)</a> (available <a href="http://www.openehr.org/ckm/" target="_blank">here</a>) is an online application that supports knowledge governance within and across the health enterprise. In practice, it is a system for collaborative development, management and publishing of a wide range of clinical knowledge resources including archetypes, templates and terminology subsets.</p>
+			<h2>Governança do Conhecimento Clínico (Clinical Knowledge Governance)</h2>
+			<p>O <a href="documentation">Clinical Knowledge Manager (CKM)</a> (disponível em <a href="http://www.openehr.org/ckm/" target="_blank">here</a>) 
+
+openEHR é uma aplicação on-line que suporta a governança do conhecimento dentro e através da corporação de Saúde. Na prática, é um sistema para desenvolvimento 
+
+colaborativo, gerenciamento e publicação de um extenso conjunto de recursos de conhecimentos clínicos incluindo arquétipos, modelos (templates) e subconjuntos de 
+
+terminologia.</p>
 			
 			
-			<h2>Clinical process including guidelines, workflow and pathways</h2>
-			<p>The aim of all this work is to support the automated processing of health information to assist people and their health care providers in achieving the best possible health outcomes. A great deal of work and research has gone into establishing the means of providing this assistance. With openEHR, it is possible for the first time to express the process statements that depend on health information (such as take prednisolone 50mg daily if the peak flow measurement drops below 200 L/min on two consecutive mornings) in formal terms. This is because openEHR has a language for querying the record that allows statements to be made based on the archetypes that specify the information.</p>
+			<h2>Processo clínico incluindo orientações, fluxo de trabalho e os caminhos</h2>
+			<p>O objetivo de todo este trabalho é apoiar o processamento automatizado de informações em saúde para assistir pessoas e seus provedores de 
+
+cuidados de saúde em obter os melhores resultados de saúde possíveis. Um grande esforço de trabalho e pesquisa foi despendido no estabelecimento de maneiras de prover 
+
+esta assistência. Com o openEHR, é possível pela primeira vez expressar declarações de processos dependentes de informações em saúde (como tomar prednisolona 50mg por 
+
+dia, se a medida de pico de fluxo cai abaixo de 200 L / min em duas manhãs consecutives) usando termos formais. Isto é em virtude do openEHR possuir uma linguagem de 
+
+recuperação de registros que permite a construção de sentenças com base em arquétipos que especificam a informação.</p>
 			
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
 		</div>	
