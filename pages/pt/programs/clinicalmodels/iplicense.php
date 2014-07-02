491a76543625483df0c1bd96da7417547d155aba
Flávio Barbosa <flavio@ciaware.com.br>
2014-07-02 16:44:42 -0300

    Tradução para português

--- pages/pt/programs/clinicalmodels/iplicense.php
+++ pages/pt/programs/clinicalmodels/iplicense.php
@@ -1,10 +1,10 @@
-﻿<?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'templates/_functions.php');
 
 //Change page name here:
-$PageName = 'Propriedade Intelectual e Licenças';
+$PageName = 'IP e Licenciamento';
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/templates/pt/_header.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header.php');
 ?>
 
 		<div id="Content">
@@ -12,46 +12,45 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/pt/_header.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			
-			<h2>Copyright</h2>
-			<p>Artefacts produced by the Clinical Program are copyrighted to the openEHR Foundation. This establishes the Foundation to be the owner of the 'moral rights' to the artefacts, on behalf of all the contributors, and for the safety of the user community.</p>
+			<h2>Direitos autorais</h2>
+			<p>Artefatos produzidos pelo Programa Clínico são protegidos pelos direitos autorais da Fundação openEHR. Isso determina que a Fundação será a proprietária dos 'direitos morais' dos artefatos, em nome de todos os colaboradores e para a segurança da comunidade de usuários.</p>
+		
 			
-			
-			<h2>Attribution</h2>
-			<p>The contributors to an artefact are recognised in a contributors section of the work.</p>
-			
-			
-			<h2>Licensing</h2>
-			<p>A standard license is attached to each artefact produced within the Clinical Program.</p>
-			<p>The license assignments are as follows:</p>
+			<h2>Atribuição</h2>
+			<p>Os contribuintes de um artefato são reconhecidos em uma seção nomeada "trabalho dos colaboradores".</p>
+
+			<h2>Licenciamento</h2>
+			<p>Uma licença padrão é anexada em cada artefato produzido no âmbito do Programa Clínico.</p>
+			<p>As atribuições da licença são as seguintes:</p>
 			
 			<table class="TableMain">
 			<tbody>
 			<tr>
-				<th class="TableMainT"> Artefact type</th>
-				<th class="TableMainT"> License for use</th>
-				<th class="TableMainT"> Comments</th>
+				<th class="TableMainT"> Tipo do artefato</th>
+				<th class="TableMainT"> Licença para uso</th>
+				<th class="TableMainT"> Comentários</th>
 			</tr>
 			<tr>
-				<td class="TableMainC"> <b>Archetypes, templates</b> </td>
+				<td class="TableMainC"> <b>Arquétipos, modelos</b> </td>
 				<td class="TableMainC"> <img src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" style="border: 1px solid black" /> <br/>
-										WORK by <a href="http://www.openEHR.org">openEHR Foundation</a> is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>. </td>
-				<td class="TableMainC"> The use of the CC-BY-SA license allows for public sharing, republishing and forking of the specifications. This acts as a safeguard against subversion of the openEHR Foundation preventing the community using the specifications in a fair way. <br/>
+										PRODUZIDO pela <a href="http://www.openEHR.org">Fundação openEHR</a>está licenciado sob<a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>. </td>
+				<td class="TableMainC"> O uso da licença CC-BY-SA permite o compartilhamento com o público, republicação e bifurcação das especificações. Isso funciona como uma salvaguarda contra a subversão da Fundação openEHR prevenindo a comunidade e usando as especificações de uma forma justa. <br/>
 				</td>
 			</tr>
 			<tr>
-				<td class="TableMainC"> <b>Terminology subsets</b> </td>
+				<td class="TableMainC"> <b>Subconjunto de terminologia</b> </td>
 				<td class="TableMainC"> <img src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" style="border: 1px solid black" /> <br/>
-										WORK by <a href="http://www.openEHR.org">openEHR Foundation</a> is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>. </td>
-				<td class="TableMainC"> <br/><br/><br/>ibid <br/><br/><br/></td>
+										PRODUZIDO pela <a href="http://www.openEHR.org">Fundação openEHR</a>está licenciado sob<a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>. </td>
+				<td class="TableMainC"> <br/><br/><br/>idem <br/><br/><br/></td>
 			</tr>
 			<tr>
-				<td class="TableMainC"> <b>Documentation</b> </td>
-				<td class="TableMainC"> <img src="http://i.creativecommons.org/l/by/3.0/88x31.png" style="border: 1px solid black" /><br/>WORK by <a href="http://www.openEHR.org">openEHR Foundation</a> is licensed under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.</td>
-				<td class="TableMainC"> <br/><br/>Documentation is intended for wide dissemination. Improvement, translation and re-use are encouraged. <br/><br/><br/> </td>
+				<td class="TableMainC"> <b>Documentação</b> </td>
+				<td class="TableMainC"> <img src="http://i.creativecommons.org/l/by/3.0/88x31.png" style="border: 1px solid black" /><br/>PRODUZIDO pela <a href="http://www.openEHR.org">Fundação openEHR</a> está licenciado sob <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.</td>
+				<td class="TableMainC"> <br/><br/>A documentação é destinada a ampla divulgação. Melhorias, traduções e re-uso são incentivados <br/><br/><br/> </td>
 			</tr>
 			</tbody>
 			</table>
 		
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
 		</div>	
-<?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/pt/_footer.php');?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'templates/_footer.php');?>
