ccbade027a7b7af3a28ef8bc6123ae8a2c52a728
jussararotzsch <jussara.macedo@gmail.com>
2014-07-09 15:31:30 -0300

    Update issuetrackers.php

--- pages/pt/programs/software/issuetrackers.php
+++ pages/pt/programs/software/issuetrackers.php
@@ -1,1 +1,19 @@
-﻿<?phprequire_once($_SERVER['DOCUMENT_ROOT'].'templates/_functions.php');//Change page name here:$PageName = 'Lista de Problemas';require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header.php');?>		<div id="Content"><!-- ------------------------------------------- Content starts here ------------------------------------------------- -->					<h1><?php echo "$PageName";?></h1>						<p>Esta parte do site está em construção.</p>		<!-- ------------------------------------------- Content ends here ------------------------------------------------- -->		</div>	<?php require_once($_SERVER['DOCUMENT_ROOT'].'templates/_footer.php');?>
+﻿<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'templates/_functions.php');
+
+//Change page name here:
+$PageName = 'Lista de Problemas';
+
+require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header.php');
+?>
+
+		<div id="Conteúdo">
+<!-- ------------------------------------------- Content starts here ------------------------------------------------- -->
+		
+			<h1><?php echo "$PageName";?></h1>
+			
+			<p>Esta parte do site está em construção.</p>
+		
+<!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
+		</div>	
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'templates/_footer.php');?>
