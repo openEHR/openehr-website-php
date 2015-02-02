6ed4fbb911d9db61f9121295ec19a97bfd588f3d
wolandscat <thomas.beale@oceaninformatics.com>
2015-02-02 09:10:50 +0000

    update election links on home page for voting phase

--- pages/home.php
+++ pages/home.php
@@ -25,13 +25,13 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_top.php');
 			<div id="NewsImage">
 				<p><img src="/files/home/board.png"><span class="HomeSubtitle">Management Board Election Feb 2015</span>
 				<br/>
-				Nominate now!
+				Nominations Closed
 				<br/>
 				<br/>
 				<a href="/about/governance_structure">What is the Management Board?</a>
 				<br/>
 				<br/>
-				<a href="http://members.openehr.org/nominations" target="_blank">Board elections page</a>
+				<a href="http://members.openehr.org/page-7745" target="_blank">Board elections page</a>
 				<br/>
 				<br/>
 				<a href="http://members.openehr.org/2015-Management-Board-Nominees" target="_blank">Current nominations</a>
