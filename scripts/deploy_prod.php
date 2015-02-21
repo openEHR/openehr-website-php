053f7e0e931dd6752352cc8ad04f6f38252238b5
wolandscat <thomas.beale@oceaninformatics.com>
2015-02-21 18:45:30 +0000

    Cosmetic changes

--- scripts/deploy_prod.php
+++ scripts/deploy_prod.php
@@ -8,7 +8,6 @@
 
     // Run the commands for output
     $output = '';
-
     foreach ($commands AS $command){
         $tmp = shell_exec($command);
         $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
--- scripts/populate_reference_models.php
+++ scripts/populate_reference_models.php
@@ -1,9 +1,4 @@
 <?php
-    /**
-     * GIT RELEASE EXTRACT WRAPPER SCRIPT
-     *
-     */
-
     // The commands
     $commands = array(
         'echo $PWD',
@@ -18,20 +13,18 @@
         $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
         $output .= htmlentities(trim($tmp)) . "\n";
     }
-
-    // Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
-    <title>GIT openEHR reference-models publishing script</title>
+    <title>openEHR reference-models publishing script</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
  .  ____  .    ____________________________
  |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git openEHR reference-models publishing Script v0.1 |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | openEHR reference-models publishing Script v0.1 |
  |___==___|  /              &copy; wolandscat 2015 |
               |____________________________|
 
