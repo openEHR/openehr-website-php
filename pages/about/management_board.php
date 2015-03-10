e25c0eeb2c4cce95fe6c0be64fd630bd04d6c8ac
wolandscat <thomas.beale@oceaninformatics.com>
2015-03-10 12:52:12 +0000

    Add more social network details.

--- pages/about/management_board.php
+++ pages/about/management_board.php
@@ -26,8 +26,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>The work has continued as a Director of the openEHR Foundation, CEO until 2012 and now as Chairman of Ocean Informatics and most recently as Chairman of the openEHR Foundation. Sam was a foundation co-Chair of the HL7 EHR Technical Committee and has worked extensively with the UK and Australian national programs to utilise and refine the openEHR method.</p>
 				<p>Member since Feb 2015.</p>
 				<p> 
-					<a href="http://www.linkedin.com/pub/sam-heard/1/49a/4b1" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/samheardoi" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://www.linkedin.com/pub/sam-heard/1/49a/4b1" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/samheardoi" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -47,8 +47,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Rong has contributed to several core openEHR design specifications, and has for many years led the openEHR Reference Java Implementation of openEHR. Rong is the lead author of Guideline Definition Language (GDL), which is formal language designed to express clinical logic for sharable decision support applications across languages and technical barriers.</p>
 				<p>Elected Feb 2015.</p>
 				<p> 
-					<a href="http://se.linkedin.com/in/ronch" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/chenron" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://se.linkedin.com/in/ronch" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/chenron" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -66,8 +66,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Tomaž has a proven track record delivering innovative turn-key solutions for large clients in telecommunications, finance and healthcare. He has been speaker at conferences including TeleManagement Forum, HIMSS and Health 2.0.</p>
 				<p>Elected Feb 2015.</p>
 				<p> 
-					<a href="http://si.linkedin.com/pub/tomaz-gornik/2/70b/473/en" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/marandlab" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://si.linkedin.com/pub/tomaz-gornik/2/70b/473/en" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/marandlab" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -86,9 +86,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Having trained in Medicine and completed PhD in Information Systems, Koray's main focus is on Electronic Health Records (EHR). He has led development of a number of successful applications. The most prominent one is the <a href="http://pathos-web.sourceforge.net">PATHOS-WEB Project</a> for reporting and analysis in anatomical pathology. Installed in some 25 large laboratories in Turkey, more than two million records have been collected since 1996. In order for health informatics to be recognised as an academic discipline he played a key role during establishment of the Department of Medical Education and Informatics at Hacettepe University. Koray then led work for the development of health informatics curriculum and a graduate program in health informatics.</p>
 				<p>Elected Feb 2015.</p>
 				<p> 
-					<a href="http://openehrnz.wordpress.com/author/atalagk/" target="_blank"><img height="24" width="24" src="/gui/social/wordpress-icon.png"/></a>
-					<a href="http://www.linkedin.com/pub/koray-atalag/0/5aa/b32" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/atalagk" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://openehrnz.wordpress.com/author/atalagk/" target="_blank"><img height="20" width="20" src="/gui/social/wordpress-icon.png"/></a>
+					<a href="http://www.linkedin.com/pub/koray-atalag/0/5aa/b32" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/atalagk" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -105,8 +105,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>His current focus is on the promotion of an open e-health platform ecosystem, with openEHR at its core, and the development of the related <a href="http://handihealth.org/introducing-handi-open-platform-demonstrator-handi-hopd/">HANDIhealth Development Platform</a>, adopted by NHS England as a key part of their Code4Health initiative.</p>
 				<p>Elected Feb 2015.</p>
 				<p> 
-					<a href="http://uk.linkedin.com/in/ianmcnicoll/" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/ianmcnicoll" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://uk.linkedin.com/in/ianmcnicoll/" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/ianmcnicoll" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
--- pages/about/program_leads.php
+++ pages/about/program_leads.php
@@ -21,9 +21,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Thomas's academic background is in Electrical Engineering (communications) and Computer Science. Since 1998, he has worked on EHR architectures and archetypes, and participated in international standards work (OMG HDTF, HL7, CEN TC/251). He is one of the founders of the openEHR Foundation, and designed the archetype formalism (ADL) and object model (AOM), now an ISO standard. He has published a number of <a href="http://wolandscat.net/publications/">papers in health informatics</a>. He has also presented widely on EHRs, e-Health and archetypes.</p>
 				<p>Lead since Jan 2012.</p>
 				<p> 
-					<a href="http://wolandscat.net" target="_blank"><img height="24" width="24" src="/gui/social/wordpress-icon.png"/></a>
-					<a href="http://uk.linkedin.com/in/thomasbeale" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/wolands_cat" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://wolandscat.net" target="_blank"><img height="20" width="20" src="/gui/social/wordpress-icon.png"/></a>
+					<a href="http://uk.linkedin.com/in/thomasbeale" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/wolands_cat" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -39,9 +39,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Dr Heather Leslie is a General Practitioner and has participated in high-level health IT strategy development, as well as taking a hands-on role in creating software applications for use by Australian primary care clinicians and consumers. She was a leader in Personal Health Record development in Australia, supporting consumers to take a more active role in their health care, and this remains a strong personal interest. Heather joined Ocean Informatics in 2005, taking on a lead role in the authoring, review and governance of openEHR archetypes and templates.</p>
 				<p>Lead since Jan 2012.</p>
 				<p> 
-					<a href="http://omowizard.wordpress.com/" target="_blank"><img height="24" width="24" src="/gui/social/wordpress-icon.png"/></a>
-					<a href="http://au.linkedin.com/in/heatherleslie/en" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/omowizard" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://omowizard.wordpress.com/" target="_blank"><img height="20" width="20" src="/gui/social/wordpress-icon.png"/></a>
+					<a href="http://au.linkedin.com/in/heatherleslie/en" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/omowizard" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -58,8 +58,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p> <a href="http://www.imt.liu.se/~erisu/">Research page</a>.</p>
 				<p>Lead since Jan 2012.</p>
 				<p> 
-					<a href="http://se.linkedin.com/in/eriksundvall/en" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/ErikSundvall" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://se.linkedin.com/in/eriksundvall/en" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/ErikSundvall" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
@@ -75,9 +75,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Having trained in Medicine and completing PhD in Information Systems, Koray's main focus is on Electronic Health Records (EHR). He has led development of a number of successful applications. The most prominent one is the <a href="http://pathos-web.sourceforge.net">PATHOS-WEB Project</a> for reporting and analysis in anatomical pathology. Installed in some 25 large laboratories in Turkey, more than two million records have been collected since 1996. In order for health informatics to be recognised as an academic discipline he played a key role during establishment of the Department of Medical Education and Informatics at Hacettepe University. Koray then led work for the development of health informatics curriculum and a graduate program in health informatics.</p>
 				<p>Lead since Jan 2012.</p>
 				<p> 
-					<a href="http://openehrnz.wordpress.com/author/atalagk/" target="_blank"><img height="24" width="24" src="/gui/social/wordpress-icon.png"/></a>
-					<a href="http://www.linkedin.com/pub/koray-atalag/0/5aa/b32" target="_blank"><img height="24" width="24" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/atalagk" target="_blank"><img height="24" width="24" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://openehrnz.wordpress.com/author/atalagk/" target="_blank"><img height="20" width="20" src="/gui/social/wordpress-icon.png"/></a>
+					<a href="http://www.linkedin.com/pub/koray-atalag/0/5aa/b32" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/atalagk" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
 				</p>
 				</td>
 			</tr>
