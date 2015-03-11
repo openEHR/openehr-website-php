721d87a93c0d3442d95815a62ce3afa32dfd2350
wolandscat <thomas.beale@oceaninformatics.com>
2015-03-11 11:36:17 +0000

    Update BoG page with status note.

--- pages/about/board_of_governors.php
+++ pages/about/board_of_governors.php
@@ -11,25 +11,10 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 			
 			<h1><?php echo "$PageName";?></h1>
-			<h2 style="margin-left:1em;">2015 Governance Refresh - status</h2>
+			<h2>2015 Governance Refresh - status</h2>
 			<p>In accordance with the openEHR Foundation Governance renewal process, and the appointment of an elected Management Board, membership of the Board of Governors is expected to change in coming weeks after consultation with Past Board Members and the wider community.</p>
 
-			<h2 style="margin-left:1em;">Dr. Sam Heard - Chair of the Board (Australia)</h2>
-			<table class="TableInvisible">
-			<tbody>
-			<tr>
-				<td style="text-align:center;"><img src="/files/about/SHeard.png"></td>
-				<td width="650px">
-				<p>Sam Heard (MBBS, MRCGP, FRACGP, FACHI) is a practicing clinician who has worked throughout his career in inner London (UK) and the Northern Territory (Australia) to assist the standardisation of health information to empower clinicians and their patients to improve health care and outcomes.</p>
-				<p>This work began with the Good European Health Record in the early 90s, continued through a long collaboration with Thomas Beale and CHIME at UCL, the establishment of Ocean Informatics as a commercial vehicle to assist in the vision and culminating in setting up the openEHR Foundation in London in 2002.</p>
-				<p>The work has continued as a Director of the openEHR Foundation, CEO until 2012 and now as Chairman of Ocean Informatics and most recently as Chairman of the openEHR Foundation. Sam was a foundation co-Chair of the HL7 EHR Technical Committee and has worked extensively with the UK and Australian national programs to utilise and refine the openEHR method.</p>
-				<p><a href="http://drsamheard.com/">Blog</a>.</p>
-				</td>
-			</tr>
-			</tbody>
-			</table>
-			
-			<h2 style="margin-left:1em;">Professor David Ingram (United Kingdom)</h2>
+			<h2 style="margin-left:1em;">Professor Emeritus David Ingram (United Kingdom)</h2>
 			<table class="TableInvisible">
 			<tbody>
 			<tr>
@@ -43,13 +28,28 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			</tbody>
 			</table>
 			
-			<h2 style="margin-left:1em;">Professor Dipak Kalra (United Kingdom)</h2>
+			<h2 style="margin-left:1em;">Sam Heard MD, MRCGP, FRACGP, FACHI (Australia)</h2>
+			<table class="TableInvisible">
+			<tbody>
+			<tr>
+				<td style="text-align:center;"><img src="/files/about/SHeard.png"></td>
+				<td width="650px">
+				<p>Dr. Sam Heard is a practicing clinician who has worked throughout his career in inner London (UK) and the Northern Territory (Australia) to assist the standardisation of health information to empower clinicians and their patients to improve health care and outcomes.</p>
+				<p>This work began with the Good European Health Record in the early 90s, continued through a long collaboration with Thomas Beale and CHIME at UCL, the establishment of Ocean Informatics as a commercial vehicle to assist in the vision and culminating in setting up the openEHR Foundation in London in 2002.</p>
+				<p>The work has continued as a Director of the openEHR Foundation, CEO until 2012 and now as Chairman of Ocean Informatics and most recently as Chairman of the openEHR Foundation. Sam was a foundation co-Chair of the HL7 EHR Technical Committee and has worked extensively with the UK and Australian national programs to utilise and refine the openEHR method.</p>
+				<p><a href="http://drsamheard.com/">Blog</a>.</p>
+				</td>
+			</tr>
+			</tbody>
+			</table>
+			
+			<h2 style="margin-left:1em;">Professor Dipak Kalra PhD, FRCGP (United Kingdom)</h2>
 			<table class="TableInvisible">
 			<tbody>
 			<tr>
 				<td style="text-align:center;"><img src="/files/about/DKalra.png"></td>
 				<td width="650px">
-				<p>Dipak Kalra, PhD, FRCGP, British, is Clinical Professor of Health Informatics at University College London, United Kingdom. He is President of the European Institute for Health Records (EuroRec) and a Director of the openEHR Foundation.</p>
+				<p>Dipak Kalra, was Clinical Professor of Health Informatics at University College London, United Kingdom. He is President of the European Institute for Health Records (EuroRec) and a Director of the openEHR Foundation.</p>
 				<p>Dipak plays a leading international role in research and development of electronic health record architectures and systems, including the requirements and models needed to ensure the robust long-term preservation of clinical meaning and protection of privacy. He leads the development of CEN and ISO standards on EHR interoperability, personal health records, EHR requirements, and has contributed to several EHR security and confidentiality standards.</p>
 				<p>Dipak is involved in European research on EHR system quality labelling, semantic interoperability and clinical knowledge discovery. He leads a EU Network of Excellence on semantic interoperability, and a project to develop a European roadmap on patient empowerment innovations. He leads the Managing Entity for a €16m Innovative Medicines Initiatives on the re-use of electronic health record information for clinical research, EHR4CR, alongside ten global pharmaceutical companies. He is also a partner in another IMI project, EMIF, on the development of a European clinical research platform federating multiple population health and cohort studies.</p>
 				</td>
