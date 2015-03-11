f0c859a2da9863c75f1bc7345779b2140b874586
wolandscat <thomas.beale@oceaninformatics.com>
2015-03-11 11:30:04 +0000

    Update BoG page with status note.

--- pages/about/board_of_governors.php
+++ pages/about/board_of_governors.php
@@ -11,7 +11,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 			
 			<h1><?php echo "$PageName";?></h1>
-			
+			<h2 style="margin-left:1em;">2015 Governance Refresh - status</h2>
+			<p>In accordance with the openEHR Foundation Governance renewal process, and the appointment of an elected Management Board, membership of the Board of Governors is expected to change in coming weeks after consultation with Past Board Members and the wider community.</p>
+
 			<h2 style="margin-left:1em;">Dr. Sam Heard - Chair of the Board (Australia)</h2>
 			<table class="TableInvisible">
 			<tbody>
