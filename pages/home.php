a3e7ef4576e9f4bfddb93e524c3e0b223814993c
wolandscat <thomas.beale@oceaninformatics.com>
2015-03-11 14:05:26 +0000

    Change Mgt Board link to target same tab.

--- pages/home.php
+++ pages/home.php
@@ -26,7 +26,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_top.php');
 				<p><img src="/files/home/board.png"><span class="HomeSubtitle">Management Board Election Feb 2015</span>
 				<br/> Voting completed 28 Feb <br/>
 				<br/> <a href="/about/news_events/community_news.php?id=120">Election Results</a> <br/>
-				<br/> <a href="/about/management_board" target="_blank">New Management Board</a> <br/>
+				<br/> <a href="/about/management_board">New Management Board</a> <br/>
 				</p>
 
 				<p><img src="/files/home/membership.png"><span class="HomeSubtitle">Membership</span>
