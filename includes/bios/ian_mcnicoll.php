6571873854adcf245f112fb9670c685dc8c94581
wolandscat <thomas.beale@oceaninformatics.com>
2015-04-08 10:02:36 +0100

    Various updates to board pages.

--- includes/bios/ian_mcnicoll.php
+++ includes/bios/ian_mcnicoll.php
@@ -4,7 +4,8 @@
 			<tr>
 				<td style="text-align:center;"><img src="/files/about/IMcnicoll.png"></td>
 				<td width="650px">
-				<p>Ian McNicoll was for many years a Glasgow GP and an Honorary Senior Research Associate with CHIME, UCL and an independent clinical informatics consultant, primarily working with <a href="http://www.openehr.org/industry_partners/ocean_informatics">Ocean Informatics</a> on openEHR-based projects across Europe. He is acknowledged as one of the UK's leading health infomatics experts. He is a member of the NHS Scotland SCIMP Working Group and serves on the committee of the BCS Primary Health Care Specialist Group.</p>
+				<p>Ian is a former Scottish GP,  and has been involved in healthcare informatics for nearly 30 years, working with and promoting openEHR technologies for the last 8 years, initially  with Ocean Informatics and latterly as an independent consultant.</p>
+				<p>His current focus is on the promotion of the HANDIhealth open e-health platform ecosystem, with openEHR at its core, and the development of a related Development Platform, adopted by NHS England as a key part of their Code4Health initiative.</p>
 				<p> 
 					<a href="http://uk.linkedin.com/in/ianmcnicoll/" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
 					<a href="http://twitter.com/ianmcnicoll" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
