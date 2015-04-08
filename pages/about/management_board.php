d34f2e1a18edf8a36707e13d8b6aef440ed02c21
wolandscat <thomas.beale@oceaninformatics.com>
2015-04-08 11:14:30 +0100

    Correct structure of Mgt board

--- pages/about/management_board.php
+++ pages/about/management_board.php
@@ -13,24 +13,21 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			
 			<h1><?php echo "$PageName";?></h1>
 			
-			<h2 id="industrypartners">Industry Partner representatives</h2>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/rong_chen.php');?>
-				<p>Elected Feb 2015.</p>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/tomaz_gornik.php');?>
-				<p>Elected Feb 2015. <em>(Co-chair)</em></p>
-			
-			<h2 id="memberreps">Membership representatives</h2>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/koray_atalag.php');?>
-				<p>Elected Feb 2015.</p>
+			<h2 id="cochairs">Co-chairs</h2>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/ian_mcnicoll.php');?>
-				<p>Elected Feb 2015. <em>(Co-chair)</em></p>
+				<p>Elected Feb 2015 as Membership representative.</p>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/tomaz_gornik.php');?>
+				<p>Elected Feb 2015 as Industry Partner representative.</p>
 			
-			<h2 id="cooptedspecialists">Co-opted members</h2>
+			<h2 id="members">Members</h2>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/rong_chen.php');?>
+				<p>Elected Feb 2015 as Industry Partner representative.</p>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/koray_atalag.php');?>
+				<p>Elected Feb 2015 as Membership representative.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/thomas_beale.php');?>
 				<p>Co-opted Mar 2015.</p>
-
-			<h2 id="bogrep">Board of Governors representative</h2>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/sam_heard.php');?>
+				<p>Board of Governors representative since Mar 2015.</p>
 
 			<p><a href="mailto:mgtboard@openehr.org">contact Management Board</a>.</p>			
 
