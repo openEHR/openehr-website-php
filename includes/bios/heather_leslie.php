c4a22641ede3dafae837e0cf917c25b03f066259
wolandscat <thomas.beale@oceaninformatics.com>
2015-04-08 12:16:49 +0100

    Fix vertical alignment of social media icons

--- includes/bios/heather_leslie.php
+++ includes/bios/heather_leslie.php
@@ -6,9 +6,9 @@
 				<td width="650px">
 				<p>Dr Heather Leslie is a General Practitioner and has participated in high-level health IT strategy development, as well as taking a hands-on role in creating software applications for use by Australian primary care clinicians and consumers. She was a leader in Personal Health Record development in Australia, supporting consumers to take a more active role in their health care, and this remains a strong personal interest. Heather joined Ocean Informatics in 2005, taking on a lead role in the authoring, review and governance of openEHR archetypes and templates.</p>
 				<p> 
-					<a href="http://omowizard.wordpress.com/" target="_blank"><img height="20" width="20" src="/gui/social/wordpress-icon.png"/></a>
-					<a href="http://au.linkedin.com/in/heatherleslie/en" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/omowizard" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://au.linkedin.com/in/heatherleslie/en" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/omowizard" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://omowizard.wordpress.com/" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/wordpress-icon.png"/>Blog</a>
 				</p>
 				</td>
 			</tr>
--- includes/bios/sam_heard.php
+++ includes/bios/sam_heard.php
@@ -8,10 +8,9 @@
 				<p>This work began with the Good European Health Record in the early 90s, continued through a long collaboration with Thomas Beale and CHIME at UCL, the establishment of Ocean Informatics as a commercial vehicle to assist in the vision and culminating in setting up the openEHR Foundation in London in 2002.</p>
 				<p>The work has continued as a Director of the openEHR Foundation, CEO until 2012 and now as Chairman of Ocean Informatics and most recently as Chairman of the openEHR Foundation. Sam was a foundation co-Chair of the HL7 EHR Technical Committee and has worked extensively with the UK and Australian national programs to utilise and refine the openEHR method.</p>
 				<p> 
-					<a href="http://www.linkedin.com/pub/sam-heard/1/49a/4b1" target="_blank"><img height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
-					<a href="http://twitter.com/samheardoi" target="_blank"><img height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
-					<a href="http://drsamheard.com/" target="_blank"><img height="20" width="20" src="/gui/social/wordpress-icon.png"/></a>
-					<a href="http://drsamheard.com/" target="_blank">Blog</a>
+					<a href="http://www.linkedin.com/pub/sam-heard/1/49a/4b1" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="http://twitter.com/samheardoi" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
+					<a href="http://drsamheard.com/" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/wordpress-icon.png"/><span>Blog</span></a>
 				</p>
 				</td>
 			</tr>
