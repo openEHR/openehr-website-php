82189557f8288a51dd544d9ef26c85b1f6fb7ed5
wolandscat <thomas.beale@oceaninformatics.com>
2015-04-12 15:20:02 +0100

    New latest windows AWB with XML-schema based XML serialisation.

--- pages/downloads/ADLworkbench/home.php
+++ pages/downloads/ADLworkbench/home.php
@@ -104,9 +104,9 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 					</tr>
 					<tr>
 						<td> Windows (32-bit)</td>
-						<td> 2.0.6.2803-alpha</td>
+						<td> 2.0.6.2808-alpha</td>
 						<td> 23 Mar 2015</td>
-						<td> <a href="/download_files/adl_workbench/adl_workbench_2.0.6.2803-alpha-windows_32bit.exe">.exe installer</a></td>
+						<td> <a href="/download_files/adl_workbench/adl_workbench_2.0.6.2808-alpha-windows_32bit.exe">.exe installer</a></td>
 						<td> Rewritten export function (Archetypes>Export); faster XML export; single-file templates; improved adlc command-line; fixed various bugs in XML output.</td>
 					</tr>
 					<tr>
