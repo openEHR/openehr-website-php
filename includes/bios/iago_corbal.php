3705b5a7e09e07a51180e5cf742a81d862f90395
wolandscat <thomas.beale@oceaninformatics.com>
2015-07-02 16:12:59 +0100

    Update Iago Corbal bio.

--- includes/bios/iago_corbal.php
+++ includes/bios/iago_corbal.php
@@ -4,7 +4,11 @@
 			<tr>
 				<td style="text-align:center;"><img height="150" src="/files/about/iago_corbal.png"></td>
 				<td width="650px">
-				<p>xxx</p>
+				<p>Iago Corbal is currently a Senior Software Architect at <a href="http://www.openehr.org/industry_partners/cambio_healthcare_systems">Cambio Healthcare Systems</a>. He has a degree on Computer Engineering from the <a href="https://www.udc.es/index.html?language=en">University of A Coruña</a>, Spain and has been involved in clinical software development since 2005, working for research institutions mainly on the area of Clinical Decision Support. He is coauthor of the <a href="https://github.com/openEHR/gdl-tools/blob/master/cds/docs/specs/gdl-specs.pdf?raw=true">Guideline Definition Language (GDL)</a> and is the main developer of the <a href="https://sourceforge.net/projects/gdl-editor/">GDL editor</a>.</p>
+				<p> 
+					<a href="https://www.linkedin.com/pub/iago-corbal/23/978/372" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
+					<a href="https://twitter.com/iago_corbal" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
+				</p>
 				</td>
 			</tr>
 			</tbody>
