475dd1d61dd14d23fc3da660eddfc1894989588c
wolandscat <thomas.beale@oceaninformatics.com>
2015-07-06 21:32:58 +0100

    Add Gustavo Bacelar bio content.

--- includes/bios/gustavo_bacelar.php
+++ includes/bios/gustavo_bacelar.php
@@ -4,7 +4,9 @@
 			<tr>
 				<td style="text-align:center;"><img height="150" src="/files/about/gustavo_bacelar.png"></td>
 				<td width="650px">
-				<p>xxx</p>
+				<p>Gustavo Bacelar, MD MBA, is currently a consultant on healthcare innovation, management, informatics, and his clinical background is in ophthalmology.
+				<p>Gustavo holds a MSc degree in Medical Informatics from University of Porto, Portugal. He is a researcher at CIDES (Department of Health Informatics and Decision Sciences) at Faculty of Medicine, University of Porto, where he lectures an online course: Introduction to the openEHR standard.</p>
+				<p>He also authored the first openEHR book: Manual de introdução à norma openEHR, which is a 'getting started b'ook about openEHR.</p>
 				</td>
 			</tr>
 			</tbody>
