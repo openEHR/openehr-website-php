62e88c4f8d12772fe468a56b3a9383c96c9a9042
wolandscat <thomas.beale@oceaninformatics.com>
2015-07-08 08:56:19 +0100

    Update gustavo_bacelar bio.

--- includes/bios/gustavo_bacelar.php
+++ includes/bios/gustavo_bacelar.php
@@ -1,10 +1,10 @@
-﻿			<h3 style="margin-left:1em;"><a name="gustavo_bacelar"></a>Gustavo Bacelar (Brazil/Portugal)</h3>
+﻿			<h3 style="margin-left:1em;"><a name="gustavo_bacelar"></a>Gustavo Bacelar MD MBA (Brazil/Portugal)</h3>
 			<table class="TableInvisible">
 			<tbody>
 			<tr>
 				<td style="text-align:center;"><img height="150" src="/files/about/gustavo_bacelar.png"></td>
 				<td width="650px">
-				<p>Gustavo Bacelar, MD MBA, is currently a consultant on healthcare innovation, management, informatics, and his clinical background is in ophthalmology.
+				<p>Gustavo Bacelar is currently a consultant on healthcare innovation, management, informatics, and his clinical background is in ophthalmology.
 				<p>Gustavo holds a MSc degree in Medical Informatics from University of Porto, Portugal. He is a researcher at CIDES (Department of Health Informatics and Decision Sciences) at Faculty of Medicine, University of Porto, where he lectures an online course: Introduction to the openEHR standard.</p>
 				<p>He also authored the first openEHR book: Manual de introdução à norma openEHR, which is a 'getting started b'ook about openEHR.</p>
 				</td>
