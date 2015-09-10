f1040762a7388a0d2727d7734ee21a83136fca03
wolandscat <thomas.beale@oceaninformatics.com>
2015-09-10 22:43:13 +1000

    Add Medinfo logo

--- pages/news_events/conferences.php
+++ pages/news_events/conferences.php
@@ -12,10 +12,16 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			<h2>MedInfo</h2>
-			<ul>
-				<li>August 2015: MedInfo 2015. <a href="/pages/news_events/conference_reports/2015-08-19_medinfo_2015">Report</a>; <a href="https://openehr.atlassian.net/wiki/display/resources/MEDINFO+2015+-+Sao+Paulo%2C+Brazil">wiki page</a>.</li>
-			</ul>
-			
+			<table class="TableInvisible">
+			<tbody>
+			<tr>
+				<td style="text-align:center;"><img src="http://medinfo2015.com/wp-content/uploads/2014/10/LOGO-MEDINFO-site-2.jpg"></td>
+				<td width="650px">
+				<p>August 2015: MedInfo 2015. <a href="/pages/news_events/conference_reports/2015-08-19_medinfo_2015">Report</a>; <a href="https://openehr.atlassian.net/wiki/display/resources/MEDINFO+2015+-+Sao+Paulo%2C+Brazil">wiki page</a>.</p>
+				</td>
+			</tr> 
+			</tbody>
+			</table>	
 					
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
