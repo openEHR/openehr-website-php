9c838b5391401c323757b7a1d9046b9fffdec422
wolandscat <thomas.beale@openehr.org>
2016-03-19 18:44:23 +0000

    Update details for Infinnity.

--- pages/industry_partners/infinnity_solutions.php
+++ pages/industry_partners/infinnity_solutions.php
@@ -27,6 +27,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			<p>InfinniPlatform is a complete toolbox for developing e-health systems. It includes EHR Data Vault, EHR Integration Server, Server Configuration and Administration Tools, Business Logic Builder/Customisation Tools, clinical applications WYSWYG UI Designer for Desktop/iOS/Android and Reporting Services.</p>
 			
 			<h2>Customers</h2>
+			<p>Infinnity is a key vendor in Europe's largest openEHR implementation project in Moscow region, <a href="http://translate.google.com/translate?sl=ru&tl=en&u=http%3A%2F%2Femias.info%2Fpress-center%2Fpress%2F2015%2F05%2F19%2Fdit-moskvy-zadacha-informatizatsii-zdravookhraneniya--sokratit-izderzhki-otrasli%2F">SIMI EMIAS (System for Integrated Medical Information)</a>.</p>
 			<p>Regional healthcare providers: Chelyabinsk, Khanty-Mansiysk and Moscow. Over 5.5 million records are managed by InfinniPlatform. Over 2000 daily concurrent users.</p>
 			<p>Local healthcare providers: 6+ clinics and hospitals with nearly 1000 users and 1 mln EHR in use</p>
 		
