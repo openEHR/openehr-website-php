624a90827e78a0c5a27402f27f80bfcd161aea2c
wolandscat <thomas.beale@openehr.org>
2016-03-29 14:52:56 +0100

    Adjust Zotero button.

--- pages/getting_involved/universities_researchcentres.php
+++ pages/getting_involved/universities_researchcentres.php
@@ -14,7 +14,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			<h1><?php echo "$PageName";?></h1>
 			
 			<h2>Current openEHR Research</h2>
-			<a href="https://www.zotero.org/groups/openehr/items" target="_blank"><img src="/files/getting_involved/zotero_logo.png"></a>
+			<p>Click below to go to the Zotero openEHR publications library.</p>
+			<a href="https://www.zotero.org/groups/openehr/items" target="_blank"><img src="/files/getting_involved/zotero_logo.png" style="width:200"></a>
 
 			<h2>Research opportunities</h2>
 			<p>Below are some of the areas that represent good opportunities for academic research.</p>
