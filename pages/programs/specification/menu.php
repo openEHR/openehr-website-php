855763a527a864feeb4604cd7ca29bb1a96af70b
wolandscat <wolandscat@gmail.com>
2016-04-08 18:27:54 +0100

    minor fix

--- pages/programs/specification/menu.php
+++ pages/programs/specification/menu.php
@@ -1,7 +1,7 @@
 ﻿<span style="line-height:150%;"><a href="/programs/specification" <?php if($PageName == 'Specification Program'){?> class="active"<?php }?> style="font-weight:900;">Specification<br/>Program</a></span>
 	<ul>
 		<li><a href="/programs/specification/releases" <?php if($PageName == 'Releases'){?> class="active"<?php }?>>Releases</a></li>
-		<li><a href="/programs/specification/editorialcommittee" <?php if($PageName == 'Specifications Editorial Committee (SEC)'){?> class="active"<?php }?>>&nbsp; Spec Ed Committee (SEC)</a></li>
+		<li><a href="/programs/specification/editorialcommittee" <?php if($PageName == 'Specifications Editorial Committee (SEC)'){?> class="active"<?php }?>>Spec Ed Committee (SEC)</a></li>
 		<li><a href="/programs/specification/governance" <?php if($PageName == 'Governance'){?> class="active"<?php }?>>Governance</a></li>
 		<li><a href="/programs/specification/changeprocess" <?php if($PageName == 'Change Process'){?> class="active"<?php }?>>Change Process</a></li>
 		<li><a href="/programs/specification/releasestrategy" <?php if($PageName == 'Release Strategy'){?> class="active"<?php }?>>Release Strategy</a></li>
