42bb870f3cf5ea867aa29461bb0602260ba8fe2e
wolandscat <wolandscat@gmail.com>
2016-04-08 13:00:49 +0100

    Setting change

--- functions/meta_db_conn.php
+++ functions/meta_db_conn.php
@@ -1,10 +1,11 @@
 <?php
-	$db_conn = "";
+/*	$db_conn = "";
 	if (basename ($_SERVER['DOCUMENT_ROOT']) == "www.openehr.org") {
 		$db_conn .= "db_conn.php";
 	}
 	else {
 		$db_conn .= "db_conn_test.php";
-	}
-	require_once (dirname($_SERVER['DOCUMENT_ROOT']) . "/" . $db_conn);
+	}*/
+	$db_conn .= "db_conn.php";
+	require_once (dirname($_SERVER['DOCUMENT_ROOT']) . "/" . $db_conn);	
 ?>
