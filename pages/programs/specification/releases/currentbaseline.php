e78163ae72d69462255c725a5a4879e3808a75a3
wolandscat <wolandscat@gmail.com>
2016-04-08 18:38:14 +0100

    Minor fixes

--- pages/programs/specification/releases/currentbaseline.php
+++ pages/programs/specification/releases/currentbaseline.php
@@ -73,44 +73,44 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 
 			<!-- ------------------------------ Specification index pages ------------------------------------------- -->
 			<h2>Specifications</h2>
-			<table class="TableMain">
+			<table class="TableBasic">
 				<tbody>
 					<tr>
-						<th class="TableMainT"> Component </th>
-						<th class="TableMainT"> Description </th>
+						<th> Component </th>
+						<th> Description </th>
 					</tr>
 
 					<tr>
-						<td class="TableMainC"> <a name="CDS"></a><a href="/releases/CDS/latest/docs/index">CDS</a></td>
-						<td class="TableMainC"> Clinical Decision Support - specifications for guidelines and related languages, including GDL.</td>
+						<td> <a name="CDS"></a><a href="/releases/CDS/latest/docs/index">CDS</a></td>
+						<td> Clinical Decision Support - specifications for guidelines and related languages, including GDL.</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="QUERY"></a><a href="/releases/QUERY/latest/docs/index">QUERY</a></td>
-						<td class="TableMainC"> Query language specifications, including AQL.</td>
+						<td> <a name="QUERY"></a><a href="/releases/QUERY/latest/docs/index">QUERY</a></td>
+						<td> Query language specifications, including AQL.</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="INTG"></a><a href="/releases/INTG/latest/docs/index">INTG</a></td>
-						<td class="TableMainC"> Integration specifications, including TDS and TDO.</td>
+						<td> <a name="INTG"></a><a href="/releases/INTG/latest/docs/index">INTG</a></td>
+						<td> Integration specifications, including TDS and TDO.</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="SM"></a><a href="/releases/SM/latest/docs/index">SM</a></td>
-						<td class="TableMainC"> Service Model - service and application programming interface (API) specifications.</td>
+						<td> <a name="SM"></a><a href="/releases/SM/latest/docs/index">SM</a></td>
+						<td> Service Model - service and application programming interface (API) specifications.</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="AM"></a><a href="/releases/AM/latest/docs/index">AM</a></td>
-						<td class="TableMainC"> Archetype Model (1.4 and 2) - Archetype formalism syntax (ADL), object model (AOM), templates (OPT), and Identification.</td>
+						<td> <a name="AM"></a><a href="/releases/AM/latest/docs/index">AM</a></td>
+						<td> Archetype Model (1.4 and 2) - Archetype formalism syntax (ADL), object model (AOM), templates (OPT), and Identification.</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="RM"></a><a href="/releases/RM/latest/docs/index">RM</a></td>
-						<td class="TableMainC"> Reference Model - the core information models of the EHR, Demographics and EHR Extract</td>
+						<td> <a name="RM"></a><a href="/releases/RM/latest/docs/index">RM</a></td>
+						<td> Reference Model - the core information models of the EHR, Demographics and EHR Extract</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="BASE"></a><a href="/releases/BASE/latest/docs/index">BASE</a></td>
-						<td class="TableMainC"> Base models and languages underpinning other specifications; includes BMM, ODIN.</td>
+						<td> <a name="BASE"></a><a href="/releases/BASE/latest/docs/index">BASE</a></td>
+						<td> Base models and languages underpinning other specifications; includes BMM, ODIN.</td>
 					</tr>
 					<tr>
-						<td class="TableMainC"> <a name="TERM"></a><a href="/releases/TERM/latest/docs/index">TERM</a></td>
-						<td class="TableMainC"> openEHR support terminology, used by the RM and other components.</td>
+						<td> <a name="TERM"></a><a href="/releases/TERM/latest/docs/index">TERM</a></td>
+						<td> openEHR support terminology, used by the RM and other components.</td>
 					</tr>
 				</tbody>
 			</table>
@@ -124,37 +124,37 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 				<a href="https://openehr.atlassian.net/browse/SPECITS/?selectedTab=com.atlassian.jira.jira-projects-plugin:issues-panel">Issues</a>
 			</p>
 
-			<table class="TableMain">
+			<table class="TableBasic">
 			<tbody>
 			<tr>
-				<th class="TableMainT"> Specification </th>
-				<th class="TableMainT"> Computable form </th>
-				<th class="TableMainT"> Description </th>
-				<th class="TableMainT"> Status </th>
+				<th> Specification </th>
+				<th> Computable form </th>
+				<th> Description </th>
+				<th> Status </th>
 			</tr>
 			<tr>
-				<td class="TableMainC"> Reference Model </td>
-				<td class="TableMainC"> <a href="/releases/trunk/its/XML-schema">XSDs</a> </td>
-				<td class="TableMainC"> XML schema 1.0 expression of the Reference Model.</td>
-				<td class="TableMainC"> stable</td>
+				<td> Reference Model </td>
+				<td> <a href="/releases/trunk/its/XML-schema">XSDs</a> </td>
+				<td> XML schema 1.0 expression of the Reference Model.</td>
+				<td> stable</td>
 			</tr>
 			<tr>
-				<td class="TableMainC"> Archetype Model 1.4 </td>
-				<td class="TableMainC"> <a href="/releases/trunk/its/XML-schema">XSDs</a> </td>
-				<td class="TableMainC"> XML schema 1.0 expression of ADL/AOM 1.4.</td>
-				<td class="TableMainC"> stable</td>
+				<td> Archetype Model 1.4 </td>
+				<td> <a href="/releases/trunk/its/XML-schema">XSDs</a> </td>
+				<td> XML schema 1.0 expression of ADL/AOM 1.4.</td>
+				<td> stable</td>
 			</tr>
 			<tr>
-				<td class="TableMainC"> Templating (OET based) </td>
-				<td class="TableMainC"> <a href="https://github.com/openEHR/java-libs/tree/master/oet-parser/src/main/xsd">OET XSD</a> | <a href="https://github.com/openEHR/java-libs/tree/master/oet-parser">Java OET parser</a> </td>
-				<td class="TableMainC"> Simple XML format for templates.</td>
-				<td class="TableMainC"> trial </td>
+				<td> Templating (OET based) </td>
+				<td> <a href="https://github.com/openEHR/java-libs/tree/master/oet-parser/src/main/xsd">OET XSD</a> | <a href="https://github.com/openEHR/java-libs/tree/master/oet-parser">Java OET parser</a> </td>
+				<td> Simple XML format for templates.</td>
+				<td> trial </td>
 			</tr>
 			<tr>
-				<td class="TableMainC"> Archetype Model 2.x </td>
-				<td class="TableMainC"> <a href="https://github.com/openEHR/specifications/tree/master/ITS/AOM2/XML-schema">XSDs</a> </td>
-				<td class="TableMainC"> XML schema 1.1 expression of ADL/AOM 2.</td>
-				<td class="TableMainC"> development</td>
+				<td> Archetype Model 2.x </td>
+				<td> <a href="https://github.com/openEHR/specifications/tree/master/ITS/AOM2/XML-schema">XSDs</a> </td>
+				<td> XML schema 1.1 expression of ADL/AOM 2.</td>
+				<td> development</td>
 			</tr>
 			</tbody>
 			</table>
@@ -167,22 +167,22 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 				<a href="https://openehr.atlassian.net/browse/SPECCNF/?selectedTab=com.atlassian.jira.jira-projects-plugin:roadmap-panel">Roadmap</a> | 
 				<a href="https://openehr.atlassian.net/browse/SPECCNF/?selectedTab=com.atlassian.jira.jira-projects-plugin:issues-panel">Issues</a>
 			</p>
-			<table class="TableMain">
+			<table class="TableBasic">
 			<tbody>
 			<tr>
-				<th class="TableMainT"> Documentary Specification </th>
-				<th class="TableMainT"> Description </th>
-				<th class="TableMainT"> Status </th>
+				<th> Documentary Specification </th>
+				<th> Description </th>
+				<th> Status </th>
 			</tr>
 			<tr>
-				<td class="TableMainC">  Archetype tooling </td>
-				<td class="TableMainC"> Conformance profiles for Archetype tooling. </td>
-				<td class="TableMainC"> <a href="http://www.openehr.org/wiki/display/oecom/openEHR+2014+Roadmap+-+Product+and+Market">wiki page</a></td>
+				<td>  Archetype tooling </td>
+				<td> Conformance profiles for Archetype tooling. </td>
+				<td> <a href="http://www.openehr.org/wiki/display/oecom/openEHR+2014+Roadmap+-+Product+and+Market">wiki page</a></td>
 			</tr>
 			<tr>
-				<td class="TableMainC"> EHR Systems </td>
-				<td class="TableMainC"> Conformance profiles for EHR system components. </td>
-				<td class="TableMainC"> <a href="http://www.openehr.org/wiki/display/oecom/openEHR+2014+Roadmap+-+Product+and+Market">wiki page</a></td>
+				<td> EHR Systems </td>
+				<td> Conformance profiles for EHR system components. </td>
+				<td> <a href="http://www.openehr.org/wiki/display/oecom/openEHR+2014+Roadmap+-+Product+and+Market">wiki page</a></td>
 			</tr>
 			</tbody>
 			</table>
--- pages/resources/menu.php
+++ pages/resources/menu.php
@@ -3,7 +3,7 @@
 				<li><a href="https://www.zotero.org/groups/openehr/items" title="Publications@Zotero" target="_blank">Publications@Zotero</a></li>
 				<li><a href="/resources/learning_centre" <?php if($pageOn == 'learning_centre.php'){?> class="active"<?php }?>>Learning Centre</a></li>
 				<li><a href="/resources/publications" <?php if($pageOn == 'publications.php'){?> class="active"<?php }?>>Publications</a></li>
-				<li><a href="/wiki/display/resources/FAQs+-+Frequently+Asked+Questions" target="_blank"> FAQs</a></li>
+				<li><a href="/wiki/display/resources/FAQs+-+Frequently+Asked+Questions" title="FAQs" target="_blank">FAQs</a></li>
 				<li><a href="https://confluence.ihtsdotools.org/display/DOC" title="SNOMED CT Document Library" target="_blank">SNOMED Library</a></li>
 				<li><a href="/resources/related_projects" <?php if($pageOn == 'related_projects.php'){?> class="active"<?php }?>>Related Projects</a></li>
 			</ul>
