faaa66934135ca8046a98f3974ddff3edc935a33
wolandscat <thomas.beale@openehr.org>
2016-04-28 12:11:50 +0100

    Improve releases menu structure.

--- pages/programs/specification/menu.php
+++ pages/programs/specification/menu.php
@@ -1,13 +1,17 @@
 ﻿<span style="line-height:150%;"><a href="/programs/specification" <?php if($PageName == 'Specification Program'){?> class="active"<?php }?> style="font-weight:900;">Specification<br/>Program</a></span>
 	<ul>
-		<li><a href="/programs/specification/releases" <?php if($PageName == 'Releases'){?> class="active"<?php }?>>Releases</a></li>
+		<li><a href="/programs/specification/releases/currentbaseline" <?php if($PageName == 'Current Baseline'){?> class="active"<?php }?>>Current Baseline</a></li>
+		<li><a href="/programs/specification/releases/latestreleases" <?php if($PageName == 'Latest Releases'){?> class="active"<?php }?>>Latest Releases</a></li>
+		<li><a href="/programs/specification/releases/historicalreleases" <?php if($PageName == 'Historical Releases'){?> class="active"<?php }?>>Historical Releases</a></li>
+		<br/>
 		<li><a href="/programs/specification/editorialcommittee" <?php if($PageName == 'Specifications Editorial Committee (SEC)'){?> class="active"<?php }?>>Spec Ed Committee (SEC)</a></li>
 		<li><a href="/programs/specification/governance" <?php if($PageName == 'Governance'){?> class="active"<?php }?>>Governance</a></li>
 		<li><a href="/programs/specification/changeprocess" <?php if($PageName == 'Change Process'){?> class="active"<?php }?>>Change Process</a></li>
 		<li><a href="/programs/specification/releasestrategy" <?php if($PageName == 'Release Strategy'){?> class="active"<?php }?>>Release Strategy</a></li>
 		<li><a href="https://openehr.atlassian.net/secure/Dashboard.jspa?selectPageId=10190" target="_blank">Issue Trackers</a></li>
-		<li><a href="/programs/specification/futuredirections" <?php if($PageName == 'Future Directions'){?> class="active"<?php }?>>Future Directions</a></li>
+		<br/>
 		<li><a href="/programs/specification/iplicense" <?php if($PageName == 'IP and Licensing'){?> class="active"<?php }?>>IP and Licensing</a></li>
 		<li><a href="/programs/specification/mailinglists" <?php if($PageName == 'Mailing Lists'){?> class="active"<?php }?>>Mailing Lists</a></li>
 		<li><a href="/wiki/display/spec/Specifications+Home" target="_blank">Wiki</a></li>
+		<li><a href="/programs/specification/futuredirections" <?php if($PageName == 'Future Directions'){?> class="active"<?php }?>>Future Directions</a></li>
 	</ul>
---
+++ pages/programs/specification/releases/latestreleases.php
@@ -0,0 +1,42 @@
+﻿<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
+
+//Change page name here:
+$PageName = 'Latest Releases';
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
+?>
+
+		<div id="Content">
+<!-- ================================= Content starts here ============================== -->
+		
+			<h1><?php echo "$PageName";?></h1>
+			
+			<table class="TableBasic">
+				<tbody>
+					<tr>
+						<th> Release </th>
+						<th> Date</th>
+						<th> Description </th>
+					</tr>
+					<tr>
+						<td> <a href="currentbaseline">Current baseline</a></td>
+						<td> (latest) </td>
+						<td> Current development version of all specifications</td>
+					</tr>
+					<tr>
+						<td> <a href="/releases/RM/Release-1.0.3/docs/index">RM Release 1.0.3</a></td>
+						<td> 15 December 2015 </td>
+						<td> <a href="https://openehr.atlassian.net/projects/SPECRM/versions/10860">23 Changes made</a>; <a href="https://openehr.atlassian.net/projects/SPECPR/versions/10060">29 Problems fixed</a></td>
+					</tr>
+					<tr>
+						<td> <a href="/releases/BASE/Release-1.0.3/docs/index">BASE Release 1.0.3</a></td>
+						<td> 15 December 2015 </td>
+						<td> <a href="https://openehr.atlassian.net/projects/SPECBASE/versions/11360">2 Changes made</a>; <a href="https://openehr.atlassian.net/issues/?jql=project%20%3D%20SPECPR%20AND%20status%20%3D%20Closed%20AND%20fixVersion%20%3D%20%22RM%20Release%201.0.3%22%20AND%20text%20~%20%22overview%22%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC">5 Problems fixed</a></td>
+					</tr>
+				</tbody>
+			</table>
+
+<!-- =========================================== Content ends here =============================================== -->
+		</div>	
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_footer.php');?>
