6b9096b8e204eb4359cc10f6e066f51f27d50785
adrianadanilakova <adriana.danilakova@gmail.com>
2016-04-28 14:26:43 +0100

    Main website fixes

--- menu/bottommenu.php
+++ menu/bottommenu.php
@@ -2,21 +2,21 @@
 			<div class="BottomMenuSection floatLeft">
 				<p class="MenuHeading">Specifications <sup style="font-size:8px; font-style:italic;">Quick Links</sup></p>
 				<ul>
-				<li><a href="/programs/specification/releases/" title="Current official release of specifications">Current Release</a></li>
-				<li><a href="/programs/specification/releases/currentbaseline" title="Specifications development baseline">Development Baseline</a></li>
-				<li><a href="https://openehr.atlassian.net/secure/Dashboard.jspa?selectPageId=10190" target="_blank" title="Specifications issue trackers (Jira)">Specs (Jira)</a></li>
-				<li><a href="/releases/1.0.2/reference-models/openEHR/BMM" target="_blank" title="Reference Model Basic Meta-Model schemas (ODIN format object model schemas)">RM BMM Schemas</a></li>
+					<li><a href="/programs/specification/releases/" title="Current official release of specifications">Current Release</a></li>
+					<li><a href="/programs/specification/releases/currentbaseline" title="Specifications development baseline">Development Baseline</a></li>
+					<li><a href="https://openehr.atlassian.net/secure/Dashboard.jspa?selectPageId=10190" target="_blank" title="Specifications issue trackers (Jira)">Specs (Jira)</a></li>
+					<li><a href="/releases/1.0.2/reference-models/openEHR/BMM" target="_blank" title="Reference Model Basic Meta-Model schemas (ODIN format object model schemas)">RM BMM Schemas</a></li>
 				</ul>
 			</div>
 
 			<div class="BottomMenuSection floatRight">
 				<p class="MenuHeading">Clinical Models <sup style="font-size:8px; font-style:italic;">Quick Links</sup></p>
 				<ul>
-				<li><a href="/ckm/" title="Clinical Knowledge Manager">CKM</a></li>
-				<li><a href="/ckm/#repositoryoverview" title="Clinical Knowledge Manager mindmap of all archetypes">CKM Archetypes Mindmap</a></li>
-				<li><a href="/ckm/#userstatistics" title="Clinical Knowledge Manager user statistics">CKM User Statistics</a></li>
-				<li><a href="/wiki/display/spec/openEHR+Terminology" title="openEHR terminology/vocabulary page">openEHR Terminology</a></li>
-				<li><a href="https://github.com/openEHR/adl-archetypes/tree/master/ADL15-reference/" target="_blank" title="Library of ADL reference archetypes for compiler testing">ADL Test Archetypes</a></li>
+					<li><a href="/ckm/" title="Clinical Knowledge Manager">CKM</a></li>
+					<li><a href="/ckm/#repositoryoverview" title="Clinical Knowledge Manager mindmap of all archetypes">CKM Archetypes Mindmap</a></li>
+					<li><a href="/ckm/#userstatistics" title="Clinical Knowledge Manager user statistics">CKM User Statistics</a></li>
+					<li><a href="/wiki/display/spec/openEHR+Terminology" title="openEHR terminology/vocabulary page">openEHR Terminology</a></li>
+					<li><a href="https://github.com/openEHR/adl-archetypes/tree/master/ADL15-reference/" target="_blank" title="Library of ADL reference archetypes for compiler testing">ADL Test Archetypes</a></li>
 				</ul>
 			</div>
 		</div>
@@ -25,24 +25,24 @@
 			<div class="BottomMenuSection floatLeft">
 				<p class="MenuHeading">Resources <sup style="font-size:8px; font-style:italic;">Learn More</sup></p>
 				<ul>
-				<li><a href="https://www.zotero.org/groups/openehr/items" title="Publications@Zotero" target="_blank">Publications@Zotero</a></li>
-				<li><a href="/resources/learning_centre">Learning Centre</a></li>
-				<li><a href="/resources/publications">Publications</a></li>	
-				<li><a href="/wiki/display/resources/FAQs+-+Frequently+Asked+Questions" target="_blank">FAQs</a></li>
-				<li><a href="/resources/white_papers">White Papers</a></li>
-				<li><a href="/resources/related_projects">Related Projects</a></li>
+					<li><a href="https://www.zotero.org/groups/openehr/items" title="Publications@Zotero" target="_blank">Publications@Zotero</a></li>
+					<li><a href="/resources/learning_centre">Learning Centre</a></li>
+					<li><a href="/resources/publications">Publications</a></li>	
+					<li><a href="/wiki/display/resources/FAQs+-+Frequently+Asked+Questions" target="_blank">FAQs</a></li>
+					<li><a href="/resources/white_papers">White Papers</a></li>
+					<li><a href="/resources/related_projects">Related Projects</a></li>
 				</ul>
 			</div>
 
 			<div class="BottomMenuSection floatRight">
 				<p class="MenuHeading">Community <sup style="font-size:8px; font-style:italic;">Follow Us</sup></p>
 				<ul>
-				<li><a href="/wiki" target="_blank">Wiki</a></li>
-				<li><a href="https://github.com/openEHR" target="_blank">GitHub</a></li>
-				<li><a href="/community/mailinglists">Mailing Lists</a></li>
-				<li><a href="http://www.youtube.com/user/openehr?feature=results_main" target="_blank">YouTube</a></li>
-				<li><a href="http://www.facebook.com/pages/openEHR/109178295780185?sk=info" target="_blank">Facebook</a></li>
-				<li><a href="http://twitter.com/#!/openehr" target="_blank">Twitter</a></li>
+					<li><a href="/wiki" target="_blank">Wiki</a></li>
+					<li><a href="https://github.com/openEHR" target="_blank">GitHub</a></li>
+					<li><a href="/community/mailinglists">Mailing Lists</a></li>
+					<li><a href="http://www.youtube.com/user/openehr?feature=results_main" target="_blank">YouTube</a></li>
+					<li><a href="http://www.facebook.com/pages/openEHR/109178295780185?sk=info" target="_blank">Facebook</a></li>
+					<li><a href="http://twitter.com/#!/openehr" target="_blank">Twitter</a></li>
 				</ul>
 			</div>
 		</div>
