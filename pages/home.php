ed2a03e40b73ca5d665d9c5eca98549aed2dea2e
wolandscat <thomas.beale@openehr.org>
2016-04-29 13:08:39 +0100

    Correct home page link to working baseline page.

--- pages/home.php
+++ pages/home.php
@@ -12,7 +12,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_top.php');
 			<a href="what_is_openehr" class="button1 col1">What is openEHR?</a>
 			<a href="who_is_using_openehr" class="button1 col1">Who is using openEHR?</a>
 			<a href="https://openehr.atlassian.net/wiki/questions" class="button1 col1" target="_blank">Questions and Answers</a>
-			<a href="/programs/specification/currentbaseline" class="button3 col2">Specifications</a>
+			<a href="/programs/specification/workingbaseline" class="button3 col2">Specifications</a>
 			<a href="/releases/trunk/UML/#Diagrams___18_1_83e026d_1423485599937_29309_4372" class="button4 col2" target="_blank">UML</a>
 			<a href="/releases/trunk/its/XML-schema/" class="button4 col2" target="_blank">XSD</a>
 			<a href="/programs/clinicalmodels" class="button3 col3">Clinical Models</a>
