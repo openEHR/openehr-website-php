2f61131c3ea530e47bc84c0a8ed6a3906493e722
wolandscat <thomas.beale@openehr.org>
2016-05-19 08:30:55 +0100

    Change Zotero links to reverse date-ordered.

--- pages/getting_involved/universities_researchcentres.php
+++ pages/getting_involved/universities_researchcentres.php
@@ -15,7 +15,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			
 			<h2>Current openEHR Research</h2>
 			<p>Click below to go to the Zotero openEHR publications library.</p>
-			<a href="https://www.zotero.org/groups/openehr/items" target="_blank"><img src="/files/getting_involved/zotero_logo.png" style="width:150px"></a>
+			<a href="https://www.zotero.org/groups/openehr/items/order/dateModified/sort/desc" target="_blank"><img src="/files/getting_involved/zotero_logo.png" style="width:150px"></a>
 
 			<h2>Research opportunities</h2>
 			<p>Below are some of the areas that represent good opportunities for academic research:</p>
