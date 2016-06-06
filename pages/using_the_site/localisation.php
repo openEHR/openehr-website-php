67964f4d0f816b5c15a001c44d1a1d800a9bfc75
wolandscat <thomas.beale@openehr.org>
2016-06-06 16:57:40 +0100

    Update localisation page.

--- pages/using_the_site/localisation.php
+++ pages/using_the_site/localisation.php
@@ -12,23 +12,43 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			
-			<p>If you want to help translate openEHR website please contact us <a href="mailto:webmaster@openehr.org">here</a>. If you want to translate to language which is already established, we will provide you contact to person who is responsible for translation to that particular language to coordinate your work.</p>
-			<p>If you want to translate to language which is not yet established, following steps will explain the process:</p>
-		
-			<p>First, you will receive spreadsheet with basic words and sentences necessary to create template of website for new language.</p>
-			<p>After you send us translation, we will create copy of the website for your language with localised templates.</p>
-			<p>Once, we inform you the templates are ready, you can fork the <a href="https://github.com/openEHR/openehr-website" target="_blank">Github openehr-website repository</a>. To do this you need to have an account on Github (just register), then you perform a 'fork' operation, which you do via the highlighted button in the screen-shot below.</p>
-			<a href="/files/using_the_site/repository_fork.png"><img src="/files/using_the_site/repository_fork.png" alt="Repository fork" title="Click to enlarge" class = "imgMiddle width50"/></a>
-			
-			<p>This creates a complete copy of the openEHR website Git repository in your own Github account. Then you will edit content of pages under the particular language section (e.g.: /pages/es for Spanish language, /pages/pt for Portuguese language). The translation job consists of replacing the content (between Contents starts here and Content ends here) in most of the pages with your language translations, as illustrated in the screen-shot below.</p>
+			<p>If you want to help translate the openEHR website please <a href="mailto:webmaster@openehr.org">contact us</a>.</p>
+
+			<h2>Updating the translation of an existing language</h2>
+			<p>If you want to translate new or changed content to a language that is already established in the website, we will provide you a contact to the person responsible for that language so as to coordinate the work. Normally you will use the 'fork/pull-request' workflow as described below to do the changes.</p>
+
+			<h2>Translating the website into a new language</h2>
+			<p>If you want to translate to a language that is not yet established in the website, the process is as follows:</p>
+			<ul>
+				<li><a href="mailto:webmaster@openehr.org">Contact us</a></li>
+				<li>We will send a spreadsheet with words and phrases necessary to create the templates of the website for the new language.</li>
+				<li>After you send us the translated spreadsheet, we will create copy of the website for your language with localised templates.</li>
+				<li>When we inform you the templates are ready, you can fork the openehr-website Git repo.</li>
+				<li>You will then make your changes, and perform a 'pull request', which will notify the webmaster and cause your changes to be integrated to the website.</li>
+			</ul>
+			<p>The latter steps are described in detail below.</p>
+
+			<h2>How the website is structured</h2>
+			<p>The website content for a given language translation exists under the directory <code>/pages/lang</code>, for example <code>/pages/es</code>. The translation work consists of copying selected parts of the EN language main site to this directory, and translating three things:</p>
+			<ul>
+				<li>the main content within a page, delimited by the 'Content starts here' / 'content ends here' lines, e.g. as per <a href ="https://github.com/openEHR/openehr-website/blob/master/pages/es/openehr_programs.php">this example</a>;</li>
+				<li>index file texts (but not hrefs), as per <a href="https://github.com/openEHR/openehr-website/blob/master/pages/es/who_is_using_openehr/index.php">this example</a>;</li>
+				<li>menu file texts (but not hrefs / file names), as per <a href="https://github.com/openEHR/openehr-website/blob/master/pages/es/who_is_using_openehr/menu.php">this example</a>.</li>
+			</ul>
 			<a href="/files/using_the_site/translation.png"><img src="/files/using_the_site/translation.png" alt="Translation" title="Click to enlarge" class = "imgMiddle width50"/></a>
-			
+			<p>Translating diagrams may require some extra effort, since diagram source files may be PPT or other formats. Please contact the webmaster for diagram translations.</p>
+
+			<h2>Github workflow</h2>
+			<p>The version control workflow you will use is known as 'fork / pull', which means that you will create (or use existing) fork of the master openEHR website repository, make your changes, and then make a 'pull request', which will cause the webmaster to integrate your changes into the master website copy.</p>
+
+			<p>You can fork the <a href="https://github.com/openEHR/openehr-website" target="_blank">Github openehr-website repository</a>. To do this you need to have an account on Github (just register), then you perform a 'fork' operation, which you do via the highlighted button in the screen-shot below.</p>
+			<a href="/files/using_the_site/repository_fork.png"><img src="/files/using_the_site/repository_fork.png" alt="Repository fork" title="Click to enlarge" class = "imgMiddle width50"/></a>
+			<p>This creates a complete copy of the openEHR website Git repository in your own Github account. You can then edit content of pages under the particular language section (e.g.: /pages/es for Spanish language, /pages/pt for Portuguese language). The translation job is as described above.</p>
 			<p>There are two ways to do the editing: either do it directly on the Github web interface {you can see an 'Edit' button in the screenshot above that will allow you to do this), or clone the repository to your machine - the normal way of working with Github. You need to get a tool called <a href="https://code.google.com/p/gitextensions/" target="_blank">GitExtensions</a> and install it. Use it to do a clone of the repository to your local machine and do the translation work. You can commit it in small steps if you want 'push' your changes from the local repository to the your Github one</p>
-			<p>Once you finished translating pages, your translations are in the forked repository on your Github account. To get the changes to flow to the master openEHR website repository, you create what is called a 'pull' request, which sends a message to the main repository owner that someone has changes you want to commit. See the screen-shot below.</p>
+			<p>Once you finished translating pages (and committed and pushed, if you used a local clone), your translations are in the forked repository on your Github account. To get the changes to flow to the master openEHR website repository, you create what is called a 'pull' request, which sends a message to the main repository owner that someone has changes you want to commit. See the screen-shot below.</p>
 			<a href="/files/using_the_site/integrating_changes.png"><img src="/files/using_the_site/integrating_changes.png" alt="Integrating Changes" title="Click to enlarge" class = "imgMiddle width50"/></a>
 			
 			<p>When administrator processes this pull request, it will cause the changes to be incorporated into the master repository.</p>
-			
 			
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
 		</div>	
