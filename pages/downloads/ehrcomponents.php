97a224daeedc855522fea70e4e1a49679ca8c6fa
wolandscat <thomas.beale@openehr.org>
2016-07-07 16:05:54 +0100

    First pass EHR components update content.

--- pages/downloads/ehrcomponents.php
+++ pages/downloads/ehrcomponents.php
@@ -2,7 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
 
 //Change page name here:
-$PageName = 'EHR Components';
+$PageName = 'EHR System Components';
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 ?>
@@ -19,31 +19,69 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<th>Project</th>
 				<th>Description</th>
 				<th>Technology</th>
-				<th>Download</th>
+				<th>Access</th>
+				<th>License</th>
+				<th>Support</th>
 			</tr>	
+
+			<tr>
+				<td rowspan="0">REST API platform</td>
+			</tr>
 			<tr>
 				<td>EhrScape</td>
-				<td>openEHR health data platform, providing a rich service framework that allows you to build innovative health apps in a web friendly environment.</td>
+				<td>openEHR health data platform, providing a rich service framework that allows you to build innovative health apps in a web friendly environment. <a href="https://www.ehrscape.com/documentation.html">(more)</a>.</td>
 				<td> Java</td>
 				<td><a href="https://www.ehrscape.com/" target="_blank">EhrScape.com</a></td>
+				<td></td>
+				<td><a href="https://www.ehrscape.com/contact.html">Support</a></td>
+			</tr>
+
+			<tr>
+				<td rowspan="0">EHR back-end persistence and services</td>
 			</tr>
 			<tr>
-				<td>adl2-core</td>
-				<td>A reference implementation of openEHR reference model, archetype model and other core ADL/AOM 2 semantics. This library is the core of ADL2 Java implementations of openEHR.</td>
-				<td> Java</td>
-				<td><a href="https://github.com/openEHR/adl2-core/" target="_blank">adl2-core@GitHub</a></td>
+				<td>EtherCIS<br/><a href="https://github.com/ethercis">GitHub</a></td>
+				<td>EtherCIS (Ethereal Clinical Information System) is an Open Source platform compatible with the openEHR standard. It is designed to allow simple interactions with clients using RESTful API and persist clinical data in a separate DB engine. <a href="http://ethercis.github.io/">(more)</a>.</td>
+				<td> Java, Postgres</td>
+				<td>VM image on request</a></td>
+				<td>Apache 2.0</td>
+				<td><a href="https://app.assembla.com/spaces/ethercis-support/support/tickets">Support</a></td>
 			</tr>
 			<tr>
-				<td>open EHR-Gen</td>
+				<td>open EHR-Gen<br/><a href="https://code.google.com/archive/p/open-ehr-gen-framework/">Google Code</a></td>
 				<td>A framework for generating medical record systems based on openEHR</td>
 				<td> Grails, Groovy, Java</td>
-				<td><a href="http://code.google.com/p/open-ehr-gen-framework/" target="_blank">Download page</a></td>
+				<td><a href="https://code.google.com/archive/p/open-ehr-gen-framework/downloads" target="_blank">Downloads</a></td>
+				<td>Apache 2.0</td>
+				<td><a href="https://code.google.com/archive/p/open-ehr-gen-framework/issues">Issues</a></td>
+			</tr>
+
+			<tr>
+				<td rowspan="0">Libraries</td>
 			</tr>
 			<tr>
-				<td>Java Reference Implementation of openEHR</td>
-				<td>A reference implementation of openEHR reference model, archetype model and other core semantics. This library is the core of most other Java implementations of openEHR.</td>
-				<td>Java</td>
-				<td><a href="http://www.openehr.org/wiki/display/projects/Java+Project+Download" target="_blank">Download page</a></td>
+				<td>adl2-core<br/><a href="https://github.com/openEHR/adl2-core/" target="_blank">GitHub</a></td>
+				<td>An implementation of the openEHR <a href="/releases/AM/latest/ADL2.html">ADL2</a> and <a href="/releases/AM/latest/AOM2.html">AOM 2</a> specifications.</td>
+				<td> Java, Antlr3</td>
+				<td></td>
+				<td>Affero GPL 3</td>
+				<td><a href="https://github.com/openEHR/adl2-core/issues">Issues</a></td>
+			</tr>
+			<tr>
+				<td>Archie<br/><a href="https://github.com/nedap/archie/" target="_blank">GitHub</a></td>
+				<td>An implementation of the openEHR <a href="/releases/AM/latest/ADL2.html">ADL2</a> and <a href="/releases/AM/latest/AOM2.html">AOM 2</a> specifications.</td>
+				<td> Java, Antlr4</td>
+				<td></td>
+				<td>Apache 2</td>
+				<td><a href="https://github.com/openEHR/adl2-core/issues">Issues</a></td>
+			</tr>
+			<tr>
+				<td>java-libs<br/><a href="https://github.com/openEHR/java-libs/" target="_blank">GitHub</a></td>
+				<td>A reference implementation of openEHR <a href="/releases/RM/latest/docs/index">Reference Model</a>, <a href="/releases/AM/latest/ADL1.4.html">ADL 1.4</a>, <a href="/releases/AM/latest/AOM1.4.html">AOM 1.4</a> specifications and other core semantics. This library is the core of most other Java implementations of openEHR.</td>
+				<td>Java, Javacc</td>
+				<td><a href="http://www.openehr.org/wiki/display/projects/Java+Project+Download" target="_blank">Downloads</a></td>
+				<td>MPL 2.0/GPL 2.0/LGPL 2.1</td>
+				<td><a href="https://github.com/openEHR/java-libs/issues">Issues</a></td>
 			</tr>
 			</tbody>
 			</table>
--- pages/downloads/menu.php
+++ pages/downloads/menu.php
@@ -3,6 +3,5 @@
 				<li><a href="/downloads/modellingtools" <?php if($pageOn == 'modellingtools.php'){?> class="active"<?php }?>>Modelling Tools</a></li>
 				<li><a href="/downloads/applications" <?php if($pageOn == 'applications.php'){?> class="active"<?php }?>>Applications</a></li>
 				<li><a href="/downloads/ds_and_guidelines" <?php if($pageOn == 'ds_and_guidelines.php'){?> class="active"<?php }?>>Guidelines and DS</a></li>
-				<li><a href="/downloads/ehrcomponents" <?php if($pageOn == 'ehrcomponents.php'){?> class="active"<?php }?>>EHR Components</a></li>
-				<li><a href="/downloads/applicationbuilding" <?php if($pageOn == 'applicationbuilding.php'){?> class="active"<?php }?>>Application Building</a></li>
+				<li><a href="/downloads/ehrcomponents" <?php if($pageOn == 'ehrcomponents.php'){?> class="active"<?php }?>>EHR System Components</a></li>
 			</ul>
