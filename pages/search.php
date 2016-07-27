309ad4b80388747cdfa08a475122d8d04039fca1
wolandscat <thomas.beale@openehr.org>
2016-07-27 10:06:41 +0100

    Add back code to pre-fill search page control with search request.

--- pages/search.php
+++ pages/search.php
@@ -17,17 +17,36 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_top.php');
 		<script type='text/javascript'>
 		google.load('search', '1', {language: 'en', style: google.loader.themes.V2_DEFAULT});
 		google.setOnLoadCallback(function() {
-		  var customSearchOptions = {};
-		  var orderByOptions = {};
-		  orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
-		  customSearchOptions['enableOrderBy'] = true;
-		  customSearchOptions['orderByOptions'] = orderByOptions;
-		  customSearchOptions['overlayResults'] = true;
-		  var customSearchControl =   new google.search.CustomSearchControl('014923816845002093082:vhhv7n9d_ao', customSearchOptions);
-		  customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-		  var options = new google.search.DrawOptions();
-		  options.setAutoComplete(true);
-		  customSearchControl.draw('cse', options);
+			var customSearchOptions = {};
+			var orderByOptions = {};
+			orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
+			customSearchOptions['enableOrderBy'] = true;
+			customSearchOptions['orderByOptions'] = orderByOptions;
+			customSearchOptions['overlayResults'] = true;
+			var customSearchControl =   new google.search.CustomSearchControl('014923816845002093082:vhhv7n9d_ao', customSearchOptions);
+			customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+			var options = new google.search.DrawOptions();
+			options.setAutoComplete(true);
+			customSearchControl.draw('cse', options);
+
+			function parseParamsFromUrl() {
+				var params = {};
+				var parts = window.location.search.substr(1).split('\x26');
+				for (var i = 0; i < parts.length; i++) {
+					var keyValuePair = parts[i].split('=');
+					var key = decodeURIComponent(keyValuePair[0]);
+					params[key] = keyValuePair[1] ?
+					decodeURIComponent(keyValuePair[1].replace(/\+/g, ' ')) :
+					keyValuePair[1];
+				}
+				return params;
+			}
+
+			var urlParams = parseParamsFromUrl();
+			var queryParamName = "q";
+			if (urlParams[queryParamName]) {
+				customSearchControl.execute(urlParams[queryParamName]);
+			}
 		}, true);
 		</script>
 
