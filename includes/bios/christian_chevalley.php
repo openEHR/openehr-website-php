be587fba2551c1ed3a166c9da9f6831ddd2bff2e
wolandscat <thomas.beale@openehr.org>
2016-09-29 15:43:22 +0100

    Add Christian Chevalley to SEC.

--- includes/bios/christian_chevalley.php
+++ includes/bios/christian_chevalley.php
@@ -6,7 +6,7 @@
 				<td>
 				<p>Christian Chevalley has over 30 years of experience in software design, development, data center architecture and IT security. He has led several developments of multimillion dollar commercial projects in the international finance, manufacturing, Telco and health care industries. He is specialized in building distributed networked systems on UNIX/Linux and mainframes, integrating a large spectrum of IT resources including micro-controllers, databases, real-time unified communications, location systems, identity management and private data protection.</p>
 				<p>For the past twelve years, Christian has been focusing on producing systems for the Healthcare arena; point of care management for aged care and acute care, telemedicine and careflow. He has been developing the EtherCIS platform based on openEHR since 2010 and has delivered a first release, which includes an open source openEHR server.</p>
-				<p>Christian has studied Computer Science and Physics at Geneva University and speaks fluently English and French.</p>
+				<p>Christian has studied Computer Science and Physics at Geneva University and speaks fluent English and French.</p>
 				</td>
 			</tr>
 			</tbody>
