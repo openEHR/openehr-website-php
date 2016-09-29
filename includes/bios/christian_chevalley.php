b20960989c47de27fc618fb651e0aa82451df416
wolandscat <thomas.beale@openehr.org>
2016-09-29 15:47:33 +0100

    Add Christian Chevalley to SEC.

--- includes/bios/christian_chevalley.php
+++ includes/bios/christian_chevalley.php
@@ -4,9 +4,9 @@
 			<tr>
 				<td><img src="/files/about/ChristianChevalley.png"></td>
 				<td>
-				<p>Christian Chevalley has over 30 years of experience in software design, development, data center architecture and IT security. He has led several developments of multimillion dollar commercial projects in the international finance, manufacturing, Telco and health care industries. He is specialized in building distributed networked systems on UNIX/Linux and mainframes, integrating a large spectrum of IT resources including micro-controllers, databases, real-time unified communications, location systems, identity management and private data protection.</p>
-				<p>For the past twelve years, Christian has been focusing on producing systems for the Healthcare arena; point of care management for aged care and acute care, telemedicine and careflow. He has been developing the EtherCIS platform based on openEHR since 2010 and has delivered a first release, which includes an open source openEHR server.</p>
-				<p>Christian has studied Computer Science and Physics at Geneva University and speaks fluent English and French.</p>
+				<p>Christian Chevalley is R&D maager at ADOC Software Development. He has over 30 years of experience in software design, development, data center architecture and IT security. He has led several developments of multimillion dollar commercial projects in the international finance, manufacturing, Telco and health care industries. He is specialized in building distributed networked systems on UNIX/Linux and mainframes, integrating a large spectrum of IT resources including micro-controllers, databases, real-time unified communications, location systems, identity management and private data protection.</p>
+				<p>For the past twelve years, Christian has been focusing on producing systems for the Healthcare arena; point of care management for aged care and acute care, telemedicine and careflow. He has been developing the <a href="https://github.com/ethercis">EtherCIS platform</a> based on openEHR since 2010 and has delivered a first release, which includes an open source openEHR server.</p>
+				<p>Christian studied Computer Science and Physics at Geneva University and speaks fluent English and French.</p>
 				</td>
 			</tr>
 			</tbody>
