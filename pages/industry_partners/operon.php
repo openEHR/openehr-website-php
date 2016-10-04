aaa067553bc481d0c6a58befb5ae05e306dea2cc
wolandscat <thomas.beale@openehr.org>
2016-10-04 10:40:45 +0100

    Add Operon as industry partner.

---
+++ pages/industry_partners/operon.php
@@ -0,0 +1,54 @@
+﻿<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
+
+//Change page name here:
+$PageName = 'Operon';
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
+?>
+
+		<div id="Content">
+<!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
+			
+			<h1><?php echo "$PageName";?></h1>
+			
+			<a href="https://operon.systems/" target="_blank"><img src="/files/industry_partners/operon_logo.png" class="IndustryLogo"></a>
+			
+			<p><em>Headquarters:</em>London, United Kingdom</p>
+			<p><em>Size:</em> 6 employees</p>
+			<p><em>Trading since:</em> 2015</p>
+			<p><a href="http://operon.systems/" target="_blank">website</a></p>
+			
+			<h2>Overview</h2>
+			<p>Operon is a new venture planning to deliver an open platform-as-a-service for health and social care based on openEHR and IHE-XDS. Our aim is to provide a self-service environment, aimed particularly at microenterprises and in-house developers building apps for health and care, who would otherwise find it difficult to deal with the complexity of the professional health and care environment. The Operon platform provides all the components need to support health and care apps. This allows app developers to use the platform to take care of the routinely demanding “plumbing”, and to focus their efforts on the unique features of their app.</p>
+
+			<h2>Solutions</h2>
+			<p>Operon provides an open platform-as-a-service for health and social care. The platform provides a range of components including:</p>
+			<ul>
+				<li>openEHR</li>
+				<li>IHE-XDS</li>
+				<li>Master Patient Index</li>
+				<li>Health Provider Registry</li>
+				<li>Authentication and Access Control</li>
+				<li>Terminology Services
+				<li>Audit</li>
+				<li>Connectors to other health and care systems</li>
+			</ul>
+
+			<p>All of our components are based on open standards. This allows apps built on our platform to be ported easily to other platforms using the same standards.</p>
+			
+			<h2>Customers</h2>
+			<p>The Operon Platform is aimed at microenterprises and small in-house development teams in health and care organisations who would otherwise struggle with the complexity of the health and care environment, providing the “plumbing” to deal with:</p>
+			<ul>
+				<li>Interoperability</li>
+				<li>Information governance,  privacy and consent management</li>
+				<li>Business continuity</li>
+				<li>Cybersecurity</li>
+			</ul>
+			<p>The Platform provides a self-service environment allowing developers to instantly provision the components and services they need and concentrate their efforts on the unique features of their app.</p>
+			<p>Customers are charged only for the resources they consume. Usage plans include a ‘starter’ tier, allowing developers to make small-scale deployments permanently free of charge.</p>
+			
+
+<!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		</div>	
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_footer.php');?>
