ef2e157680bc92235f4a26cdbba08e279db8e9ff
wolandscat <thomas.beale@openehr.org>
2016-12-07 04:57:53 +1100

    Remove Jussara Rotzsch from BoG page.

--- pages/about/board_of_governors.php
+++ pages/about/board_of_governors.php
@@ -11,16 +11,11 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 			
 			<h1><?php echo "$PageName";?></h1>
-			<h2>2015 Governance Refresh - status</h2>
-			<p>In accordance with the openEHR Foundation Governance renewal process, and the appointment of an elected Management Board, membership of the Board of Governors is expected to change in coming weeks after consultation with Past Board Members and the wider community.</p>
-
 			<h2>Members</h2>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/david_ingram.php');?>
 				<p>Member since inception.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/sam_heard.php');?>
 				<p>Member since inception.</p>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/jussara_rotzsch.php');?>
-				<p>Member since 2012.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/bill_aylward.php');?>
 				<p>Member since 2013.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/thomas_beale.php');?>
