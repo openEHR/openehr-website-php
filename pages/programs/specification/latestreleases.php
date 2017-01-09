be75e42fe7deacf92fad6c10012256f2a403111f
wolandscat <thomas.beale@openehr.org>
2017-01-09 22:01:07 +0000

    Add AM Release-2.0.6

--- pages/programs/specification/latestreleases.php
+++ pages/programs/specification/latestreleases.php
@@ -20,6 +20,11 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 						<th> Description </th>
 					</tr>
 					<tr>
+						<td> <a href="/releases/AM/Release-2.0.6/docs/index" target="_blank">AM Release 2.0.6</a></td>
+						<td> 07 January 2017 </td>
+						<td> <a href="https://openehr.atlassian.net/projects/SPECAM/versions/10963" target="_blank">26 Changes made</a>; <a href="https://openehr.atlassian.net/projects/SPECPR/versions/11073" target="_blank">23 Problems fixed</a></td>
+					</tr>
+					<tr>
 						<td> <a href="/releases/RM/Release-1.0.3/docs/index" target="_blank">RM Release 1.0.3</a></td>
 						<td> 15 December 2015 </td>
 						<td> <a href="https://openehr.atlassian.net/projects/SPECRM/versions/10860" target="_blank">23 Changes made</a>; <a href="https://openehr.atlassian.net/projects/SPECPR/versions/10060" target="_blank">29 Problems fixed</a></td>
