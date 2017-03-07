02fe55989cc87c8610f97131490797aa1418c43b
wolandscat <thomas.beale@openehr.org>
2017-03-07 20:11:59 -0300

    Add China Affiliate news.

--- pages/news_events/affiliate_news/index.php
+++ pages/news_events/affiliate_news/index.php
@@ -12,7 +12,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			<h2>Meetings</h2>
-			<p><a href="/pages/news_events/affiliates/2016_11_25_openehr_china">Using and Promoting openEHR in China in 2016.</a>.</p>
+			<p><a href="/pages/news_events/affiliate_news/2016_11_25_openehr_china">Using and Promoting openEHR in China in 2016.</a>.</p>
 					
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
