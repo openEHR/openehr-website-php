238705cd01344ef35ce11609e126cedaf8c530ac
wolandscat <thomas.beale@openehr.org>
2017-03-08 09:35:55 -0300

    Rename Affiliates to Local Affiliates.

--- pages/news_events/local_affiliate_news/index.php
+++ pages/news_events/local_affiliate_news/index.php
@@ -18,7 +18,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 			<p><a href="http://www.openehr.org.cn:8080/MyOpenEHRWeb/index.jsp">openEHR China website</a></p>
 			<p>News</p>
 			<ul>
-				<li><a href="/pages/news_events/affiliate_news/2016_11_25_openehr_china">Using and Promoting openEHR in China in 2016.</a>.</li>
+				<li><a href="/pages/news_events/local_affiliate_news/2016_11_25_openehr_china">Using and Promoting openEHR in China in 2016.</a>.</li>
 			</ul>
 					
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
