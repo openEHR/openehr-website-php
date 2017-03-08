a984a90762424c43b5be188990a6d0d892d0c7bd
wolandscat <thomas.beale@openehr.org>
2017-03-08 08:25:57 -0300

    Better affiliate jump page.

--- pages/news_events/affiliate_news/index.php
+++ pages/news_events/affiliate_news/index.php
@@ -12,11 +12,14 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			<h2>openEHR Brazil</h2>
-			<p><a href="http://openehr.org.br/">openEHR Brazil website</a>.</p>
+			<p><a href="http://openehr.org.br/">openEHR Brazil website</a></p>
 
 			<h2>openEHR China</h2>
-			<p><a href="http://www.openehr.org.cn:8080/MyOpenEHRWeb/index.jsp">openEHR China website</a>.</p>
-			<p><a href="/pages/news_events/affiliate_news/2016_11_25_openehr_china">Using and Promoting openEHR in China in 2016.</a>.</p>
+			<p><a href="http://www.openehr.org.cn:8080/MyOpenEHRWeb/index.jsp">openEHR China website</a></p>
+			<p>News</p>
+			<ul>
+				<li><a href="/pages/news_events/affiliate_news/2016_11_25_openehr_china">Using and Promoting openEHR in China in 2016.</a>.</li>
+			</ul>
 					
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
