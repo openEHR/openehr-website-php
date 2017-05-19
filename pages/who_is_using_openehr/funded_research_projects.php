6355623c299006a17ab14ab580f1b5d925f5adf7
wolandscat <thomas.beale@openehr.org>
2017-05-19 13:45:22 +0100

    Updates to funded research projects page.

--- pages/who_is_using_openehr/funded_research_projects.php
+++ pages/who_is_using_openehr/funded_research_projects.php
@@ -14,6 +14,38 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			
 			<p>The following table indicates known funded research projects using openEHR. Please <a href="/about/contacts">contact us</a>, to notify of other projects.</p>
 			
+			<h2>Current Projects</h2>
+			<table class="TableBasic">
+			<tbody>
+			<tr>
+				<th>Location</th>
+				<th>Project</th>
+				<th>Description</th>
+				<th>Vendor(s) / consortium</th>
+				<th>Status</th>
+			</tr>
+			<tr>
+				<td rowspan="2">
+					<img src="/files/who_is_using_openehr/Flag_of_Europe.svg.png" alt="EU flag" title="EU flag" style="width:80px;"><p>European Union</p>
+				</td>
+				<td>
+					<a href="http://www.myaircoach.eu/myaircoach/">MY AIR COACH</a><br/>myAirCoach project aims to support asthma patients to control their disease through mHealth. myAirCoach is an EU funded project under Horizon 2020 (grant agreement No. 643607), and has 12 partners. The project started on the 1st of January 2015 and will last 3 years.
+				</td>
+				<td>
+					myAirCoach aims to develop an asthma monitoring system using personalised mHealth. One of the main goals of the project is to help patients manage their health through user-friendly tools that will increase their awareness of their clinical state as well as the adherence and effectiveness of medical treatment they follow.
+				</td>
+				<td>
+					<a href="/industry_partners/medrecord">MEDrecord, Netherlands</a>
+				</td>
+				<td>
+					to end 2018
+				</td>
+			</tr>
+			</tbody>
+			</table>
+
+
+			<h2>Past Projects</h2>
 			<table class="TableBasic">
 			<tbody>
 			<tr>
@@ -30,7 +62,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				27 partners.</td>
 				<td>The CHIRON project intends to combine state-of-the art technologies and innovative solutions into an integrated framework designed for an 
 				effective and person-centric health management along the complete care cycle. Will monitor 100 patients with atrial fibrillation during 2013.</td>
-				<td><a href="http://www.medvision360.com/?lang=en">MedVision360 (Zorg Gemak), Netherlands</a></td>
+				<td><a href="/industry_partners/medrecord">MEDrecord, Netherlands</a></td>
 				<td>March 2010 - end 2013</td>
 			</tr>
 			<tr>
@@ -40,8 +72,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<td>The aim of the MobiGuide project is to develop an intelligent decision-support system for patients with chronic illnesses. The system accompanies 
 				the patients wherever they go and helps them and their care providers in managing their illness,
 				whether they are at home, at work, out and about or travelling abroad on holiday or for business.</td>
-				<td><a href="http://www.medvision360.com/?lang=en">MedVision360 (Zorg Gemak), Netherlands</a></td>
-				<td>ends 2013</td>
+				<td><a href="/industry_partners/medrecord">MEDrecord, Netherlands</a></td>
+				<td>ended 2013</td>
 			</tr>
 			</tbody>
 			</table>
