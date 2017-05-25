635a9ec896f83a0e4da63ccd057916182155800e
wolandscat <thomas.beale@openehr.org>
2017-05-25 12:50:31 -0600

    Remove Critical from website.

--- pages/industry_partners/menu.php
+++ pages/industry_partners/menu.php
@@ -2,7 +2,6 @@
 			<ul>
 				<li><a href="/industry_partners/cambio_healthcare_systems" <?php if($pageOn == 'cambio_healthcare_systems.php'){?> class="active"<?php }?>>Cambio Healthcare Systems</a></li>
 				<li><a href="/industry_partners/code24" <?php if($pageOn == 'code24.php'){?> class="active"<?php }?>>Code24</a></li>
-				<li><a href="/industry_partners/critical_software" <?php if($pageOn == 'critical_software.php'){?> class="active"<?php }?>>Critical Software</a></li>
 				<li><a href="/industry_partners/dips" <?php if($pageOn == 'dips.php'){?> class="active"<?php }?>>DIPS</a></li>
 				<li><a href="/industry_partners/infinnity_solutions" <?php if($pageOn == 'infinnity_solutions.php'){?> class="active"<?php }?>>Infinnity Solutions</a></li>
 				<li><a href="/industry_partners/marand" <?php if($pageOn == 'marand.php'){?> class="active"<?php }?>>Marand</a></li>
