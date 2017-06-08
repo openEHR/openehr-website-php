97903d185355d2b9eb5fbbe10357b51fc042e0ea
wolandscat <thomas.beale@openehr.org>
2017-06-08 15:58:21 +0100

    Simpler link to UML

--- pages/home.php
+++ pages/home.php
@@ -13,7 +13,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_top.php');
 			<a href="who_is_using_openehr" class="button1 col1">Who is using openEHR?</a>
 			<a href="https://openehr.atlassian.net/wiki/questions" class="button1 col1" target="_blank">Questions and Answers</a>
 			<a href="/programs/specification/workingbaseline" class="button3 col2">Specifications</a>
-			<a href="/releases/trunk/UML/#Diagrams___18_1_83e026d_1423485599937_29309_4372" class="button4 col2" target="_blank">UML</a>
+			<a href="/releases/trunk/UML/" class="button4 col2" target="_blank">UML</a>
 			<a href="/releases/trunk/its/XML-schema/" class="button4 col2" target="_blank">XSD</a>
 			<a href="/programs/clinicalmodels" class="button3 col3">Clinical Models</a>
 			<a href="http://www.openehr.org/ckm/" class="button4 col3" target="_blank">CKM</a>
