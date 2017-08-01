0034b1c8cbf72a386a8fc7bf6184ba51b274a363
wolandscat <thomas.beale@openehr.org>
2017-08-01 14:52:02 +0100

    Add Apperta videos to Learning Centre page.

--- pages/resources/learning_centre.php
+++ pages/resources/learning_centre.php
@@ -8,7 +8,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 ?>
 
 		<div id="Content">
-<!-- ------------------------------------------- Content starts here ------------------------------------------------- -->
+<!-- ------------------------------------------ Content starts here ----------------------------------------------- -->
 		
 			<h1><?php echo "$PageName";?></h1>
 			
@@ -19,17 +19,23 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			<p><a href="http://virtualcare.pt/curso_openehr/">INTRODUÇÃO AO OPENEHR</a></p>
 
 			<h2>openEHR Primers</h2>
-			<iframe width="400" height="300" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26WRld2BF3BM2sdqrCBT5f3C" frameborder="0" allowfullscreen></iframe>
-			<br/>
-			<h2>ADL 1.5 training</h2>
-			<iframe width="400" height="300" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26VSw4RQb10cVaRB7Ln73wfM&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
-			<br/>
-			<br/>
+			<h3>DIPS Conference, Norway, 20013 - T Beale</h3>
+			<iframe width="250" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26WRld2BF3BM2sdqrCBT5f3C" frameborder="0" allowfullscreen></iframe>
+
+			<h3>Apperta Foundation - 2017</h3>
+			<iframe width="250" src="https://www.youtube.com/embed/BUwGNXppdbw?list=PLNxHSK29ViKI1foDYNUbToJuFdhwKzx4U" frameborder="0" allowfullscreen></iframe>
+			<iframe width="250" src="https://www.youtube.com/embed/ZyJv-tzS344" frameborder="0" allowfullscreen></iframe>
+			<iframe width="250" src="https://www.youtube.com/embed/7tIYjR7ykMo" frameborder="0" allowfullscreen></iframe>
+
+			<h2>ADL 2 training (series)</h2>
+			<iframe width="250" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26VSw4RQb10cVaRB7Ln73wfM&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
+			
+			
 			<h2>ADL Workbench tutorial</h2>
-			<iframe width="400" height="300" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26U6PGezBrDD2QPeQL8tQYTZ&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
-			<br/>
+			<iframe width="250" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26U6PGezBrDD2QPeQL8tQYTZ&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
+			
 			<h2>MedInfo 2007 - David Ingram</h2>
-			<iframe width="400" height="300" src="//www.youtube.com/embed/Nlw1LrDmv7c?list=UU0rSnWtVmuvr4MJ4vHC_kQA" frameborder="0" allowfullscreen></iframe>
+			<iframe width="250" src="//www.youtube.com/embed/Nlw1LrDmv7c?list=UU0rSnWtVmuvr4MJ4vHC_kQA" frameborder="0" allowfullscreen></iframe>
 			
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
 		</div>	
