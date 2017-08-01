d60b37340f5ee752a3030236797f4248d3150b62
wolandscat <thomas.beale@openehr.org>
2017-08-01 16:54:41 +0100

    Reformat learning centre page a bit.

--- pages/resources/learning_centre.php
+++ pages/resources/learning_centre.php
@@ -15,9 +15,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			<h2>openEHR Presentations</h2>
 			<p><a href="http://www.slideshare.net/search/slideshow?searchfrom=header&q=openehr">Slideshare</a></p>
 
-			<h2>openEHR Course Material - Portuguese</h2>
-			<p><a href="http://virtualcare.pt/curso_openehr/">INTRODUÇÃO AO OPENEHR</a></p>
-
 			<h2>openEHR Primers</h2>
 			<h3>DIPS Conference, Norway, 20013 - T Beale</h3>
 			<iframe width="250" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26WRld2BF3BM2sdqrCBT5f3C" frameborder="0" allowfullscreen></iframe>
@@ -27,14 +24,18 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			<iframe width="250" src="https://www.youtube.com/embed/ZyJv-tzS344" frameborder="0" allowfullscreen></iframe>
 			<iframe width="250" src="https://www.youtube.com/embed/7tIYjR7ykMo" frameborder="0" allowfullscreen></iframe>
 
-			<h2>ADL 2 training (series)</h2>
+			<h2>Tutorials</h2>
+			<h3>openEHR Course Material - Portuguese</h3>
+			<p><a href="http://virtualcare.pt/curso_openehr/">INTRODUÇÃO AO OPENEHR</a></p>
+
+			<h3>ADL 2 training (series)</h3>
 			<iframe width="250" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26VSw4RQb10cVaRB7Ln73wfM&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
 			
-			
-			<h2>ADL Workbench tutorial</h2>
+			<h3>ADL Workbench tutorial</h3>
 			<iframe width="250" src="http://www.youtube.com/embed/videoseries?list=PLhWi0RtmG26U6PGezBrDD2QPeQL8tQYTZ&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
 			
-			<h2>MedInfo 2007 - David Ingram</h2>
+			<h2>Background</h2>
+			<h3>MedInfo 2007 - David Ingram</h3>
 			<iframe width="250" src="//www.youtube.com/embed/Nlw1LrDmv7c?list=UU0rSnWtVmuvr4MJ4vHC_kQA" frameborder="0" allowfullscreen></iframe>
 			
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
