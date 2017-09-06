f5027983062783711e954f9275744f7ad7fca85b
wolandscat <thomas.beale@openehr.org>
2017-09-06 11:27:08 +0200

    Improve about menu readability slightly.

--- pages/about/menu.php
+++ pages/about/menu.php
@@ -1,15 +1,17 @@
-<span>About Us</span>
+<span>About</span>
 			<ul>
 				<li><a href="/about/foundation" <?php if($pageOn == 'foundation.php'){?> class="active"<?php }?>>Foundation</a></li>
 				<li><a href="/about/membership" <?php if($pageOn == 'membership.php'){?> class="active"<?php }?>>Membership</a></li>
 				<li><a href="/about/ambassadors" <?php if($pageOn == 'ambassadors.php'){?> class="active"<?php }?>>Ambassadors</a></li>
 				<li><a href="/about/program_leads" <?php if($pageOn == 'program_leads.php'){?> class="active"<?php }?>>Program Leads</a></li>
+				<br>
 
 				<li><a href="/about/governance_structure" <?php if($pageOn == 'governance_structure.php'){?> class="active"<?php }?>>Governance Structure</a></li>
 				<li><a href="/about/board_of_governors" <?php if($pageOn == 'board_of_governors.php'){?> class="active"<?php }?>>Board of Governors</a></li>
 				<li><a href="/about/management_board" <?php if($pageOn == 'management_board.php'){?> class="active"<?php }?>>Management Board</a></li>
 
 				<li><a href="/about/past_board_members" <?php if($pageOn == 'past_board_members.php'){?> class="active"<?php }?>>Past Board Members</a></li>
+				<br>
 
 				<li><a href="/about/origins" <?php if($pageOn == 'origins.php'){?> class="active"<?php }?>>Origins of openEHR</a></li>
 				<li><a href="/about/intellectual_property" <?php if($pageOn == 'intellectual_property.php'){?> class="active"<?php }?>>Intellectual Property</a></li>
