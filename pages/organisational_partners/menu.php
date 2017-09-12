6d245cbb7c6ae55235e94876bfb6e683b8f6bc4f
wolandscat <thomas.beale@openehr.org>
2017-09-12 17:53:52 +0100

    Correct URL typo.

--- pages/organisational_partners/menu.php
+++ pages/organisational_partners/menu.php
@@ -1,4 +1,4 @@
-<span><a href="/organisastional_partners/index" <?php if($pageOn == 'index.php'){?> class="active"<?php }?>>openEHR Organisational Partners</a></span>
+<span><a href="/organisational_partners/index" <?php if($pageOn == 'index.php'){?> class="active"<?php }?>>openEHR Organisational Partners</a></span>
 			<ul>
 				<li><a href="/organisational_partners/norway" <?php if($pageOn == 'norway.php'){?> class="active"<?php }?>>Norwegian Nasjonal IKT</a></li>
 			</ul>
