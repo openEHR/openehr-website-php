97d6825d15e0840db154fd313fb02b7f2106c89f
wolandscat <thomas.beale@openehr.org>
2017-09-29 09:19:05 +0100

    Fix AWB missing page.

--- pages/downloads/ADLworkbench/browsing_and_compiling.php
+++ pages/downloads/ADLworkbench/browsing_and_compiling.php
@@ -1,10 +1,10 @@
 ﻿<?php
-require_once($_SERVER['DOCUMENT_ROOT'].'templates/_functions.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
 
 //Change page name here:
 $PageName = 'Browsing and Compiling';
 
-require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header_english.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 ?>
 
 		<div id="Content">
@@ -248,4 +248,4 @@ require_once($_SERVER['DOCUMENT_ROOT'].'templates/_header_english.php');
 		
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
 		</div>	
-<?php require_once($_SERVER['DOCUMENT_ROOT'].'templates/_footer.php');?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_footer.php');?>
