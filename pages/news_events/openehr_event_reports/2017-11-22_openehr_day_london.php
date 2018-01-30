e3e3ab502da846f350abf3b3a3b5087653e6cb95
wolandscat <thomas.beale@openehr.org>
2018-01-30 16:10:58 +0000

    Update openEHR events and conference reports pages.

--- pages/news_events/openehr_event_reports/2017-11-22_openehr_day_london.php
+++ pages/news_events/openehr_event_reports/2017-11-22_openehr_day_london.php
@@ -11,9 +11,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 <!-- -- ------------------------------------------ Content starts here ----------------------------------------------- -- -->
 		
 		<h1><?php echo "$PageName";?></h1>
-        <div class="banner-openehr text-center">
-            <img src="open-ehr-day.jpg" alt="Open EHR" />
-        </div>
         <div class="bg2 section">
             <div class="container-fluid max-width  section-bottom ">
                 <h2>Videos</h2>
@@ -21,7 +18,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Introduction and keynote "The Postmodern EHR"</h4>
-                            <iframe src="https://www.youtube.com/embed/-rhB8bDfdbU?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/-rhB8bDfdbU?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30  ">Introduction, Jon Hoeksma CEO at Digital Health Intelligence</div>
                             <div class="offset-top-30  ">Keynote: "The Postmodern EHR", Tomaz Gornik, CEO, Marand d.o.o.</div>
                             <div class="offset-top-30  ">Presentation is available <a href="http://bit.ly/2kagjy7">here</a>.</div>
@@ -30,7 +27,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Medtronic GET READY® - openEHR-based Patient Engagement</h4>
-                            <iframe src="https://www.youtube.com/embed/Rcv-3aSfPoA?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/Rcv-3aSfPoA?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Use Case: Medtronic, Dr Matic Meglic, Digital Health Director, EMEA "GET READY® - openEHR-based Patient Engagement for Improved Outcomes"</div>
                             <div class="offset-top-30">Presentantion is avaliable <a href="http://bit.ly/2nfGmow">here</a></div>
                         </div>
@@ -40,7 +37,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Use Case: Eurotransplant</h4>
-                            <iframe src="https://www.youtube.com/embed/sKSMEIarcnI?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/sKSMEIarcnI?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Use Case: Eurotransplant Wouter Zanen, Program Manager, Netherlands</div>
                             <div class="offset-top-30">“CORE: Future-proof architecture based on openEHR ”</div>
                             <div class="offset-top-30">Presentation is avaliable <a href="http://bit.ly/2BwMhbW">here</a>.</div>
@@ -49,7 +46,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Use Case: Managing heart transplant patients</h4>
-                            <iframe src="https://www.youtube.com/embed/2hbmiLs14vM?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/2hbmiLs14vM?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Use Case:Managing heart transplant patients Tina V. Vavpotič, Product Manager, Marand</div>
                             <div class="offset-top-30">“Remote monitoring of heart transplant patients using a care coordination framework”</div>
                             <div class="offset-top-30">Presentations is avaliable <a href="http://bit.ly/2Blwv2D">here</a>.</div>
@@ -60,7 +57,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Use Case: Genomics England</h4>
-                            <iframe src="https://www.youtube.com/embed/geX5RO9woPI?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/geX5RO9woPI?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Use Case: Genomics England, Jon Reed, Partner, IT Health Partnership</div>
                             <div class="offset-top-30">"A federated approach to collecting clinical data for the 100,000 Genomes Project”</div>
                             <div class="offset-top-30">Presentation is avaliable <a href="http://bit.ly/2k9Jlhc">here</a>.</div>
@@ -69,7 +66,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Use Case: NHS Scotland</h4>
-                            <iframe src="https://www.youtube.com/embed/ZWzqti-_qIA?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/ZWzqti-_qIA?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Use Case: NHS Scotland, Dr Paul Miller, SCIMP Clinical Lead</div>
                             <div class="offset-top-30">"Decision Support for NHS Scotland - why openEHR GDL?"</div>
                             <div class="offset-top-30">Presentation is available <a href="http://bit.ly/2Bywiub">here</a>.</div>
@@ -80,7 +77,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Use Case: Leeds Person Held Record</h4>
-                            <iframe src="https://www.youtube.com/embed/A89fEyeRl7k?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/A89fEyeRl7k?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Leeds City Council, David Maidment, Head of ICT Strategy, Architecture &amp; Commissioning</div>
                             <div class="offset-top-30">"Leeds Person Held Record - building on an open platform”</div>
                             <div class="offset-top-30">Presentation available <a href="http://bit.ly/2BlLQAg">here</a>.</div>
@@ -89,7 +86,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Use Case: Plymouth Hospitals NHS Trust</h4>
-                            <iframe src="https://www.youtube.com/embed/upv2Ln9mXsQ?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/upv2Ln9mXsQ?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Use Case: OPENeP, Andy Blofield, CIO, Plymouth Hospitals NHS Trust</div>
                             <div class="offset-top-30">"OPENeP - An Open Standards Approach to an Integrated Digital Care Record”</div>
                             <div class="offset-top-30">Presentation is available <a href="http://bit.ly/2k8plf2">here</a>.</div>
@@ -100,7 +97,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Introduction to Archetypes Modelling</h4>
-                            <iframe src="https://www.youtube.com/embed/snvm21SZoho?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/snvm21SZoho?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">Introduction to Archetypes Modelling, Dr Ian McNicoll, co-chair, openEHR Foundation</div>
                             <div class="offset-top-30">Presentation is available <a href="http://bit.ly/2BwGlje">here</a>.</div>
                         </div>
@@ -108,7 +105,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">openEHR Platforms, Tools and Applications</h4>
-                            <iframe src="https://www.youtube.com/embed/zFSq66FkULA?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/zFSq66FkULA?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">openEHR Platforms, Tools and Applications</div>
                             <div class="offset-top-30">Tomaz Gornik CEO Marand</div>
                             <div class="offset-top-30">Anže Droljc Product Manager, Marand</div>
@@ -120,7 +117,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">openEHR Future - Task planning</h4>
-                            <iframe src="https://www.youtube.com/embed/Zv5rwm4TYzU?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/Zv5rwm4TYzU?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">"openEHR Future: Task Planning"</div>
                             <div class="offset-top-30">Thomas Beale, Specification lead, openEHR Foundation</div>
                             <div class="offset-top-30">Presentation is available <a href="http://bit.ly/2k9ARGM">here</a>.</div>
@@ -130,7 +127,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
                     <div class="col-md-6 text-center ">
                         <div class="well ehight">
                             <h4 class="font-weight-300 ">Panel</h4>
-                            <iframe src="https://www.youtube.com/embed/3-O9AXadC50?rel=0" frameborder="0" width="100%" height="315"></iframe>
+                            <iframe src="https://www.youtube.com/embed/3-O9AXadC50?rel=0" frameborder="0" height="315"></iframe>
                             <div class="offset-top-30">openEHR Day - November 22, 2017 - Panel: "Advantages and challenges of introducing open platforms in the NHS"</div>
                         </div>
                     </div>
--- pages/news_events/openehr_event_reports/index.php
+++ pages/news_events/openehr_event_reports/index.php
@@ -14,7 +14,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 			<h2>Roadmap Meeting</h2>
 			<p>September 2014: Roadmap Meeting. <a href="2014-09-16_openehr_meeting_report">Report</a>; <a href="http://www.openehr.org/wiki/display/oecom/September+2014+Roadmap+Meeting">wiki page</a>.</p>
 
-			<h1><?php echo "$PageName";?></h1>
 			<h2>openEHR Days</h2>
 			<p>November 2017, London. <a href="2017-11-22_openehr_day_london">Presentations</a>.</p>
 					
