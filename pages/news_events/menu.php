869fa5c408fabba86ec84e0472aa28d3dc10c156
wolandscat <thomas.beale@openehr.org>
2018-01-30 18:21:26 +0000

    Update openEHR events and conference reports pages.

--- pages/news_events/menu.php
+++ pages/news_events/menu.php
@@ -4,8 +4,8 @@
 				<li><a href="/news_events/community_news" <?php if($PageName == 'Community News'){?> class="active"<?php }?>>Community News</a></li>
 				<li><a href="/news_events/local_affiliate_news" <?php if($PageName == 'Local Affiliate News'){?> class="active"<?php }?>>Local Affiliate News</a></li>
 				<li><a href="/news_events/foundation_news" <?php if($PageName == 'Foundation News'){?> class="active"<?php }?>>Foundation News</a></li>
-				<li><a href="/news_events/events" <?php if($PageName == 'Events News'){?> class="active"<?php }?>>Events</a></li>
-				<li><a href="/news_events/releases" <?php if($PageName == 'Releases News'){?> class="active"<?php }?>>Releases</a></li>
+				<li><a href="/news_events/events" <?php if($PageName == 'Events News'){?> class="active"<?php }?>>Events News</a></li>
+				<li><a href="/news_events/releases" <?php if($PageName == 'Releases News'){?> class="active"<?php }?>>Releases News</a></li>
 				<li><a href="/news_events/posting_news" <?php if($pageOn == 'posting_news.php'){?> class="active"<?php }?>>Posting News</a></li>
 				<br/>
 				<li><a href="/news_events/clinical_model_news" <?php if($PageName == 'CKM Activity'){?> class="active"<?php }?>>CKM Activity</a></li>
