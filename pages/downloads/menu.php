b09b725d5604fa3d94fc2636eef02cad00020a14
wolandscat <thomas.beale@openehr.org>
2018-04-25 08:26:07 +0100

    Modify downloads links.

--- pages/downloads/menu.php
+++ pages/downloads/menu.php
@@ -1,7 +1,6 @@
 <span>Tools</span>
 			<ul>
 				<li><a href="/downloads/modellingtools" <?php if($pageOn == 'modellingtools.php'){?> class="active"<?php }?>>Modelling Tools</a></li>
-				<li><a href="/downloads/applications" <?php if($pageOn == 'applications.php'){?> class="active"<?php }?>>Applications</a></li>
 				<li><a href="/downloads/ds_and_guidelines" <?php if($pageOn == 'ds_and_guidelines.php'){?> class="active"<?php }?>>Guidelines and DS</a></li>
-				<li><a href="/downloads/ehrcomponents" <?php if($pageOn == 'ehrcomponents.php'){?> class="active"<?php }?>>EHR System Components</a></li>
+				<li><a href="/downloads/ehrcomponents" <?php if($pageOn == 'ehrcomponents.php'){?> class="active"<?php }?>>CDR System Components</a></li>
 			</ul>
