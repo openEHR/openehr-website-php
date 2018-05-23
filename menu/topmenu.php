b2a866eafc505c984e81747983df6c27aef16e15
wolandscat <thomas.beale@openehr.org>
2018-05-23 09:08:45 +0100

    Remove Localisation from website menu.

--- menu/topmenu.php
+++ menu/topmenu.php
@@ -2,21 +2,20 @@
 		<ul id="menu" class="menu">
 		
 			<li><a href="/home">Home</a>
-			<ul>
-				<li><a href="/what_is_openehr">What is openEHR?</a></li>
-				<li><a href="/who_is_using_openehr">Who is using openEHR?</a></li>
-				<li><a href="/industry_partners">openEHR Industry Partners</a></li>
-				<li><a href="/organisational_members">openEHR Organisational Members</a></li>
-			</ul>
+				<ul>
+					<li><a href="/what_is_openehr">What is openEHR?</a></li>
+					<li><a href="/who_is_using_openehr">Who is using openEHR?</a></li>
+					<li><a href="/industry_partners">openEHR Industry Partners</a></li>
+					<li><a href="/organisational_members">openEHR Organisational Members</a></li>
+				</ul>
 			</li>
 			
 			<li><a href="/openehr_programs">Programs</a>
-			<ul>
-				<li><a href="/programs/specification">Specification Program</a></li>
-				<li><a href="/programs/clinicalmodels">Clinical Models Program</a></li>
-				<li><a href="/programs/software">Software Program</a></li>
-				<li><a href="/programs/localisation">Localisation Program</a></li>
-			</ul>
+				<ul>
+					<li><a href="/programs/specification">Specification Program</a></li>
+					<li><a href="/programs/clinicalmodels">Clinical Models Program</a></li>
+					<li><a href="/programs/software">Software Program</a></li>
+				</ul>
 			</li>
 			
 			<li><?php include $_SERVER['DOCUMENT_ROOT'].'/pages/getting_involved/menu.php'?>
