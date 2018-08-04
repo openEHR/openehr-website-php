117eece69c349088f11e974266df7843cebb0354
wolandscat <thomas.beale@openehr.org>
2018-08-04 10:48:14 +0200

    Update management board page after July 2018 election.

---
+++ includes/bios/xudong_lu.php
@@ -0,0 +1,13 @@
+			<h3 id="xudong_lu">Prof. Xudong Lu (China)</h3>
+			<table class="TableInvisible">
+			<tbody>
+			<tr>
+				<td><img src="/files/about/xudong_lu.png"></td>
+				<td>
+				<p>Huilong Duan is professor of biomedical informatics in Zhejiang University of China, associate director of the Biomedical Information Technology Institute of ZJU, and the General Secretary of the Chinese Medical Software Association.</p>
+				<p>He is also a visiting research professor at the Technical University of Eindhoven, Netherlands.</p>
+				<p>His research interests are medical informatics, clinical decision support systems and big data analytics in medicine.</p>
+				</td>
+			</tr>
+			</tbody>
+			</table>
--- pages/about/management_board.php
+++ pages/about/management_board.php
@@ -20,16 +20,22 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 				<p>Elected Feb 2015 as Industry Partner representative.</p>
 			
 			<h2 id="members">Members</h2>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/rong_chen.php');?>
-				<p>Elected Feb 2015 as Industry Partner representative.</p>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/koray_atalag.php');?>
-				<p>Elected Feb 2015 as Membership representative.</p>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/bjorn_naess.php');?>
+				<p>Elected Jul 2018 as Industry Partner representative.</p>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/xudong_lu.php');?>
+				<p>Elected Jul 2018 as Membership representative.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/silje_ljosland_bakke.php');?>
 				<p>Co-opted Aug 2016.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/thomas_beale.php');?>
 				<p>Co-opted Mar 2015.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/sam_heard.php');?>
 				<p>Board of Governors representative since Mar 2015.</p>
+
+			<h2 id="previous_members">Past Members</h2>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/rong_chen.php');?>
+				<p>Industry Partner representative Feb 2015 - Jul 2018.</p>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/koray_atalag.php');?>
+				<p>Membership representative Feb 2015 - Jul 2018.</p>
 
 			<h2 id="contact">Contact</h2>
 			<p><a href="mailto:mgtboard@openehr.org">Contact Management Board</a></p>			
