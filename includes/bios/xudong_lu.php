81b9eaad76e1e8ee8d7bc1f3319a6eb4ef70f485
wolandscat <thomas.beale@openehr.org>
2018-08-06 19:33:34 +0200

    Update management board page after July 2018 election.

--- includes/bios/xudong_lu.php
+++ includes/bios/xudong_lu.php
@@ -4,7 +4,7 @@
 			<tr>
 				<td><img src="/files/about/xudong_lu.png"></td>
 				<td>
-				<p>Huilong Duan is professor of biomedical informatics in Zhejiang University of China, associate director of the Biomedical Information Technology Institute of ZJU, and the General Secretary of the Chinese Medical Software Association.</p>
+				<p>Xudong Lu is professor of biomedical informatics in Zhejiang University of China, associate director of the Biomedical Information Technology Institute of ZJU, and the General Secretary of the Chinese Medical Software Association.</p>
 				<p>He is also a visiting research professor at the Technical University of Eindhoven, Netherlands.</p>
 				<p>His research interests are medical informatics, clinical decision support systems and big data analytics in medicine.</p>
 				</td>
