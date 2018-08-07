6b3bec63fdfd05f99c8a5f45eec5c390a3764921
wolandscat <thomas.beale@openehr.org>
2018-08-07 09:15:34 +0200

    Update management board page after July 2018 election.

--- includes/bios/xudong_lu.php
+++ includes/bios/xudong_lu.php
@@ -4,8 +4,8 @@
 			<tr>
 				<td><img src="/files/about/xudong_lu.png"></td>
 				<td>
-				<p>Xudong Lu is professor of biomedical informatics in Zhejiang University of China, associate director of the Biomedical Information Technology Institute of ZJU, and the General Secretary of the Chinese Medical Software Association.</p>
-				<p>He is also a visiting research professor at the Technical University of Eindhoven, Netherlands.</p>
+				<p>Xudong Lu is professor of biomedical informatics in Zhejiang University of China, associate director of the Biomedical Information Technology Institute of ZJU. He also serves as General Secretary of the Chinese Medical Software Association which has large influence on health IT domain in China.</p>
+				<p>Xudong has been involved in healthcare informatics for over 20 years and led several major Chinese national projects including "High-end EMR Technologies and System Development" and "National Registy of Cadiovascular Diseases".  He is a visiting guest professor at the Technical University of Eindhoven, where he co-supervised several PhD students on clinical pathway analytics and clinical decision support.</p>
 				<p>His research interests are medical informatics, clinical decision support systems and big data analytics in medicine.</p>
 				</td>
 			</tr>
