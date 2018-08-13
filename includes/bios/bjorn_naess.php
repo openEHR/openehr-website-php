ace9124f9f51a1ca0a15e8a9b4709e4654ead985
wolandscat <thomas.beale@openehr.org>
2018-08-13 16:33:25 +0100

    Update Bjorn Naess bio info.

--- includes/bios/bjorn_naess.php
+++ includes/bios/bjorn_naess.php
@@ -4,7 +4,8 @@
 			<tr>
 				<td><img src="/files/about/BjornNaess.png"></td>
 				<td>
-				<p>Bjørn received his M. Sc. in Telecommunications from the Norwegian university of science and technology, NTNU. Before this he worked some years as a physiotherapist. He has been working in e-health in various areas, starting as a developer of a national booking solution for medical services in Norway. Later he worked on integration and interop between systems and organizations. More recently he has focused on implementation of openEHR in DIPS Arena, the next generation of the DIPS EHR solution. His contributions to this work has included both openEHR evangelism and on the specification and implementation of the DIPS openEHR server.</p>
+				<p>Has implemented openEHR systems since 2010 and has been a member of the specification editorial committee since 2015. Bjorn believes in an open platform approach and that the openEHR community is the best approach to make a global approach to better health data, which is needed to be able to provide better healthcare services and for the research for new treatments.</p>
+				<p>He received his M. Sc. in Telecommunications from the Norwegian university of science and technology, NTNU. Before this he worked some years as a physiotherapist. He has been working in e-health in various areas, starting as a developer of a national booking solution for medical services in Norway. Later he worked on integration and interop between systems and organizations. More recently he has focused on implementation of openEHR in DIPS Arena, the next generation of the DIPS EHR solution. His contributions to this work has included both openEHR evangelism and on the specification and implementation of the DIPS openEHR server.</p>
 				</td>
 			</tr>
 			</tbody>
