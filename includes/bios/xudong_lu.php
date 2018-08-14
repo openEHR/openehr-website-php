5a9cd637914c031070f14baa19404be3cf0d1fc1
wolandscat <thomas.beale@openehr.org>
2018-08-14 16:38:41 +0100

    Adjust bio of Xudong Lu.

--- includes/bios/xudong_lu.php
+++ includes/bios/xudong_lu.php
@@ -4,7 +4,7 @@
 			<tr>
 				<td><img src="/files/about/xudong_lu.png"></td>
 				<td>
-				<p>Xudong Lu is professor of biomedical informatics in Zhejiang University of China.</p>
+				<p>Xudong is professor of medical informatics in Zhejiang University of China. His research focuses are EHR, CDSS and big data analytics. He has been working in the area of healthcare informatics for over 20 years and led several major Chinese national projects including “High-end EMR Technologies and System Development”.  He is also a visiting guest professor at the Technical University of Eindhoven, where he co-supervised several PhD students on clinical pathway analytics and clinical decision support.  Currently he serves as General Secretary of the Chinese Medical Software Association which has large influence on health IT domain in China.</p>
 				</td>
 			</tr>
 			</tbody>
