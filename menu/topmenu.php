09a7993ec6e2374fcad6f1ea2f34229c5b65f173
wolandscat <thomas.beale@openehr.org>
2018-08-15 09:06:12 +0100

    Fix a broken link in the home page menu.

--- menu/topmenu.php
+++ menu/topmenu.php
@@ -6,7 +6,7 @@
 					<li><a href="/what_is_openehr">What is openEHR?</a></li>
 					<li><a href="/who_is_using_openehr">Who is using openEHR?</a></li>
 					<li><a href="/industry_partners">openEHR Industry Partners</a></li>
-					<li><a href="/organisational_members">openEHR Organisational Members</a></li>
+					<li><a href="/organisational_partners">openEHR Organisational Partners</a></li>
 				</ul>
 			</li>
 			
