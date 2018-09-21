9da121aa04dc9c3e082496657475a517c096e743
wolandscat <thomas.beale@openehr.org>
2018-09-21 12:32:48 +0100

    Add Oslo 13 Sep 2018 openEHR day links.

--- pages/news_events/openehr_event_reports/index.php
+++ pages/news_events/openehr_event_reports/index.php
@@ -11,13 +11,14 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_english.php');
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		
 			<h1><?php echo "$PageName";?></h1>
+			<h2>openEHR Days</h2>
+			<p>13 September 2018, Oslo. <a href="https://www.youtube.com/playlist?list=PLnMJHuL5FeHYAEdH-eJT_tX2Ag9Fwc5So">Presentations</a>.</p>
+			<p>20 March 2018, Helsinki / 21 March 2018, Stockholm. <a href="http://pages.tieto.com/LP-GL-PHCW-2017-05-openEHR_LP-event.html">Presentations</a>.</p>
+			<p>November 2017, London. <a href="2017-11-22_openehr_day_london">Presentations</a>.</p>
+					
 			<h2>Roadmap Meeting</h2>
 			<p>September 2014: Roadmap Meeting. <a href="2014-09-16_openehr_meeting_report">Report</a>; <a href="http://www.openehr.org/wiki/display/oecom/September+2014+Roadmap+Meeting">wiki page</a>.</p>
 
-			<h2>openEHR Days</h2>
-			<p>November 2017, London. <a href="2017-11-22_openehr_day_london">Presentations</a>.</p>
-			<p>March 20 Helsinki / March 21 Stockholm. <a href="http://pages.tieto.com/LP-GL-PHCW-2017-05-openEHR_LP-event.html">Presentations</a>.</p>
-					
 <!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
 		</div>	
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_footer.php');?>
