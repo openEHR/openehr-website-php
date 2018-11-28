3cac0a463284e7373eaee438ecb5cd9faf7a6dcb
wolandscat <thomas.beale@openehr.org>
2018-11-28 11:33:20 +0000

    Correct BOG page and Sam Heard bio.

--- includes/bios/sam_heard.php
+++ includes/bios/sam_heard.php
@@ -6,7 +6,7 @@
 				<td>
 				<p>Sam Heard is a practicing clinician who has worked throughout his career in inner London (UK) and the Northern Territory (Australia) to assist the standardisation of health information to empower clinicians and their patients to improve health care and outcomes.</p>
 				<p>This work began with the Good European Health Record in the early 90s, continued through a long collaboration with Thomas Beale and CHIME at UCL, the establishment of Ocean Informatics as a commercial vehicle to assist in the vision and culminating in setting up the openEHR Foundation in London in 2002.</p>
-				<p>The work has continued as a Director of the openEHR Foundation, CEO until 2012 and now as Chairman of Ocean Informatics and most recently as Chairman of the openEHR Foundation. Sam was a foundation co-Chair of the HL7 EHR Technical Committee and has worked extensively with the UK and Australian national programs to utilise and refine the openEHR method.</p>
+				<p>The work has continued as a Director of the openEHR Foundation, and CEO until 2012 and current board Chair of Ocean Health Systems. Sam was a foundation co-Chair of the HL7 EHR Technical Committee and has worked extensively with the UK and Australian national programs to utilise and refine the openEHR method.</p>
 				<p> 
 					<a href="http://www.linkedin.com/pub/sam-heard/1/49a/4b1" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/linkedin-follow.png"/></a>
 					<a href="http://twitter.com/samheardoi" target="_blank"><img class="middle" height="20" width="20" src="/gui/social/twitter-follow.png"/></a>
--- pages/about/board_of_governors.php
+++ pages/about/board_of_governors.php
@@ -13,13 +13,15 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 			<h1><?php echo "$PageName";?></h1>
 			<h2>Members</h2>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/david_ingram.php');?>
-				<p>Member since inception.</p>
+				<p>Member since 2000; current chair.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/sam_heard.php');?>
-				<p>Member since inception.</p>
-			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/bill_aylward.php');?>
-				<p>Member since 2013.</p>
+				<p>Member since 2000.</p>
 			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/thomas_beale.php');?>
 				<p>Member since October 2016.</p>
+
+			<h2>Past Members</h2>
+			<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/bios/bill_aylward.php');?>
+				<p>Member 2013 - November 2018.</p>
 			
 			<h2>Contact</h2>
 			<p><a href="mailto:govboard@openehr.org">Contact Board of Governors</a></p>
