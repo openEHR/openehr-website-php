c961d72a1e12243defb78419ef8e98b731d03909
wolandscat <thomas.beale@openehr.org>
2019-01-11 16:53:10 +0000

    Update IP page with membership levels.

--- pages/industry_partners/index.php
+++ pages/industry_partners/index.php
@@ -12,117 +12,139 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
 		
 			<h1><?php echo "$PageName";?></h1>
 			<p>The openEHR Industry Partners are a group of companies implementing openEHR who have agreed to materially support the Foundation.  Industry Partners support the foundation with resources, both human and financial; and work on many aspects of openEHR, but in particular are focussed on product compliance and quality. To that end they are responsible for the <a href="/releases/CNF/latest/docs/index">conformance specifications for openEHR.</a></p>
-			<table class="TableInvisible">
-			<tbody>
 
-			<!-- Accenda -->
-			<tr>
-				<td><a href="/industry_partners/accenda"><img src="/files/industry_partners/accenda_logo.png"></a></td>
-				<td><h2>Accenda</h2>
-					<p>Accenda Limited is a UK-based software consulting business.  We help our clients streamline processes, reduce waste and improve efficiencies. We work closely with NHS Digital to help deliver solutions based on cutting-edge technology. We have a 'sleeves-rolled-up' approach to engaging on projects and are passionate about delivering solutions that deliver real value for patients, clinicians and stakeholders.</p>
-				<a href="/industry_partners/accenda">Learn more</a>
-				</td>
-			</tr>
+			<h2>Diamond Partner</h2>
+				<table class="TableInvisible">
+				<tbody>
+					<!-- Cambio -->
+					<tr>
+						<td><a href="/industry_partners/cambio_healthcare_systems"><img src="/files/industry_partners/cambio_logo.png"></a></td>
+						<td><h3>Cambio Healthcare Systems</h3>
+							<p>Cambio Healthcare Systems is an e-Health company with approximately 370 employees who provide the Cambio COSMIC healthcare system for comprehensive, easy-to-use solutions to meet the healthcare needs of all patients. Since 1993, Cambio has worked tirelessly to develop open, flexible and comprehensive IT support for healthcare. Development has taken place in constant close co-operation with those who work in health care - doctors, nurses, administrators and IT-technichians.</p>
+						<a href="/industry_partners/cambio_healthcare_systems">Learn more</a></td>
+					</tr>
 
-			<!-- Cambio -->
-			<tr>
-				<td><a href="/industry_partners/cambio_healthcare_systems"><img src="/files/industry_partners/cambio_logo.png"></a></td>
-				<td><h2>Cambio Healthcare Systems</h2>
-					<p>Cambio Healthcare Systems is an e-Health company with approximately 370 employees who provide the Cambio COSMIC healthcare system for comprehensive, easy-to-use solutions to meet the healthcare needs of all patients. Since 1993, Cambio has worked tirelessly to develop open, flexible and comprehensive IT support for healthcare. Development has taken place in constant close co-operation with those who work in health care - doctors, nurses, administrators and IT-technichians.</p>
-				<a href="/industry_partners/cambio_healthcare_systems">Learn more</a></td>
-			</tr>
+					<!-- DIPS -->
+					<tr>
+						<td><a href="/industry_partners/dips"><img src="/files/industry_partners/dips_logo.png"></a></td>
+						<td><h3>DIPS</h3>
+						<p>DIPS ASA is the largest supplier of electronic patient record systems to Norwegian hospitals, the largest supplier of Radiology Information Systems and the second largest supplier of Laboratory Information Systems. DIPS has group and framework agreements with the health trusts organised under the regional health authorities for South-Eastern Norway, Western Norway and Northern Norway. DIPS also has customers in Denmark and Sweden.</p>
+						<a href="/industry_partners/dips">Learn more</a></td>
+					</tr>
 
-			<!-- Code24 -->
-			<tr>
-				<td><a href="/industry_partners/code24"><img src="/files/industry_partners/code24_logo.png"></a></td>
-				<td><h2>Code24</h2>
-				<p>Code24 B.V. is an innovative software development company located in Alkmaar in the Netherlands. Driven by great passion and dedication, our efforts are devoted entirely to the development of IT applications that can be used in the healthcare sector. We specialise in providing smart, web-based solutions for care providers that are also easy to use. Thanks to those solutions, they are able to rely upon a single, manageable and comprehensive overview of all of their patient's and client's health details, even if that information originates from a variety of systems.</p>
-				<a href="/industry_partners/code24">Learn more</a></td>
-			</tr>
+					<!-- Tieto -->
+					<tr>
+						<td><a href="/industry_partners/tieto"><img src="/files/industry_partners/tieto_logo.png"></a></td>
+						<td><h3>Tieto</h3>
+						<p>Tieto is the leading Nordic software and services company whose goal is to be the customer's first choice for business renewal. In a rapidly changing world, every bit of information can be used to provide new value. Tieto aims to capture the significant opportunities of the data-driven world and turn them into lifelong value for people, business and society.</p>
+						<p>Healthcare is an important strategic area for Tieto.</p>
+						<a href="/industry_partners/tieto">Learn more</a></td>
+					</tr>
+				</tbody>
+				</table>
 
-			<!-- CORE Consulting -->
-			<tr>
-				<td><a href="/industry_partners/core_consulting"><img src="/files/industry_partners/core_consulting_logo.png"></a></td>
-				<td><h2>CORE Consulting</h2>
-				<p>CORE Consulting's provides architectural vision and critical advice to support the development and implementation of eHealth solutions in Brazil. CORE is involved in several ehealth projects, including the proposal to create a national center for terminology and openEHR clinical knowledge artifacts. CORE takes part in several standardization committees and also in technical and scientific communities, standards such as OpenEHR, IHE (PIX / MPI, PDQ, XDS.b), HL7, SNOMED-CT.</p>
-				<a href="/industry_partners/core_consulting">Learn more</a></td>
-			</tr>
+			<h2>Platinum Partner</h2>
 
-			<!-- DIPS -->
-			<tr>
-				<td><a href="/industry_partners/dips"><img src="/files/industry_partners/dips_logo.png"></a></td>
-				<td><h2>DIPS</h2>
-				<p>DIPS ASA is the largest supplier of electronic patient record systems to Norwegian hospitals, the largest supplier of Radiology Information Systems and the second largest supplier of Laboratory Information Systems. DIPS has group and framework agreements with the health trusts organised under the regional health authorities for South-Eastern Norway, Western Norway and Northern Norway. DIPS also has customers in Denmark and Sweden.</p>
-				<a href="/industry_partners/dips">Learn more</a></td>
-			</tr>
+			<h2>Gold Partner</h2>
+				<table class="TableInvisible">
+				<tbody>
+					<!-- Marand -->
+					<tr>
+						<td><a href="/industry_partners/marand"><img src="/files/industry_partners/marand_logo.png"></a></td>
+						<td><h3>Marand</h3>
+						<p>Marand is one of the leading Slovenian company in the field of information and communication technologies and information support business processes. Marand is a customer-centric solution provider in healthcare offering products including Think!Med Clinical&trade;, Think!Med EHR Platform and Think!Med Preventive. All these solutions are based on state of the art technology and industry standards.</p>
+						<a href="/industry_partners/marand">Learn more</a></td>
+					</tr>
 
-			<!-- Inidus -->
-			<tr>
-				<td><a href="/industry_partners/inidus"><img src="/files/industry_partners/inidus_logo.png"></a></td>
-				<td><h2>Inidus</h2>
-				<p>inidus provides an open, secure, scalable health and care platform that enables developers to build health and care applications using open standards including HL7 FHIR, openEHR, SNOMED CT and IHE-XDS, making your data and applications portable and extensible.  The inidus platform enables rapid development allowing you to use any modern programming language, and our platform services to build integrated and scalable applications. Hosted in UK owned and located data centers meeting UK government and NHS standards for information governance and cyber security and offering N3 connectivity the inidus platform helps you meet your customer's requirements for connectivity and cyber security.</p>
-				<a href="/industry_partners/inidus">Learn more</a></td>
-			</tr>
+					<!-- Ocean -->
+					<tr>
+						<td><a href="/industry_partners/ocean_health_systems"><img src="/files/industry_partners/ocean_logo.png"></a></td>
+						<td><h3>Ocean Health Systems</h3>
+						<p>Ocean Health Systems is a recognised leader in e-health strategy, semantic interoperability and the shared EHR. Founded in 1998, Ocean Informatics is a unique company, comprising an experienced engineering team with strong clinical expertise. Collectively the Ocean team has over 100 person-years of health informatics experience. Ocean's vision is fully integrated and highly computable health data organised around the patient care process, served from high-availability infrastructure to point of care, reporting and analytics applications.</p>
+						<a href="/industry_partners/ocean_health_systems">Learn more</a></td>
+					</tr>
+				</tbody>
+				</table>
 
-			<!-- IRYO -->
-			<tr>
-				<td><a href="/industry_partners/iryo"><img src="/files/industry_partners/iryo_logo.png"></a></td>
-				<td><h2>IRYO</h2>
-				<p>Iryo is a Slovenia-based IT company focused on improving the user experience of all stakeholders in the healthcare system. The Iryo system is using openEHR for increasing interoperability in healthcare, zero-knowledge data storage for data security and patient enforceable privacy. The company is building a global open-sourced permisionless protocol for healthcare solutions.</p>
-				<a href="/industry_partners/iryo">Learn more</a></td>
-			</tr>
+			<h2>Silver Partner</h2>
 
-			<!-- Infinnity -->
-			<tr>
-				<td><a href="/industry_partners/infinnity_solutions"><img src="/files/industry_partners/infinnity_logo.png"></a></td>
-				<td><h2>Infinnity Solutions</h2>
-				<p>Infinnity Solutions is a Russian company who develop innovative software solutions in the field of health. The main areas of software development are integrated electronic medical record, medical information systems and mobile medical applications. Since 2006, Infinnity Solutions is concerned with improving the efficiency of hospitals and health care systems as a whole.</p>
-				<a href="/industry_partners/infinnity_solutions">Learn more</a></td>
-			</tr>
+			<h2>Bronze Partner</h2>
+				<table class="TableInvisible">
+				<tbody>
+					<!-- Code24 -->
+					<tr>
+						<td><a href="/industry_partners/code24"><img src="/files/industry_partners/code24_logo.png"></a></td>
+						<td><h3>Code24</h3>
+						<p>Code24 B.V. is an innovative software development company located in Alkmaar in the Netherlands. Driven by great passion and dedication, our efforts are devoted entirely to the development of IT applications that can be used in the healthcare sector. We specialise in providing smart, web-based solutions for care providers that are also easy to use. Thanks to those solutions, they are able to rely upon a single, manageable and comprehensive overview of all of their patient's and client's health details, even if that information originates from a variety of systems.</p>
+						<a href="/industry_partners/code24">Learn more</a></td>
+					</tr>
 
-			<!-- Marand -->
-			<tr>
-				<td><a href="/industry_partners/marand"><img src="/files/industry_partners/marand_logo.png"></a></td>
-				<td><h2>Marand</h2>
-				<p>Marand is one of the leading Slovenian company in the field of information and communication technologies and information support business processes. Marand is a customer-centric solution provider in healthcare offering products including Think!Med Clinical&trade;, Think!Med EHR Platform and Think!Med Preventive. All these solutions are based on state of the art technology and industry standards.</p>
-				<a href="/industry_partners/marand">Learn more</a></td>
-			</tr>
+					<!-- CORE Consulting -->
+					<tr>
+						<td><a href="/industry_partners/core_consulting"><img src="/files/industry_partners/core_consulting_logo.png"></a></td>
+						<td><h3>CORE Consulting</h3>
+						<p>CORE Consulting's provides architectural vision and critical advice to support the development and implementation of eHealth solutions in Brazil. CORE is involved in several ehealth projects, including the proposal to create a national center for terminology and openEHR clinical knowledge artifacts. CORE takes part in several standardization committees and also in technical and scientific communities, standards such as OpenEHR, IHE (PIX / MPI, PDQ, XDS.b), HL7, SNOMED-CT.</p>
+						<a href="/industry_partners/core_consulting">Learn more</a></td>
+					</tr>
 
-			<!-- MEDrecord 
-			<tr>
-				<td><a href="/industry_partners/medrecord"><img src="/files/industry_partners/medrecord_logo.png"></a></td>
-				<td><h2>MEDrecord BV</h2>
-				<p>MEDrecord provides a ground-breaking eHealth platform completely based on the openEHR standard. We have a long history in the eHealth landscape with significant expertise on security, continuous development and semantic interoperability. For developers and integrators we provide advanced, easy-to-use APIs for the development of clinically validated apps.</p>
-				<a href="/industry_partners/medrecord">Learn more</a></td>
-			</tr>
-			-->
+					<!-- Infinnity -->
+					<tr>
+						<td><a href="/industry_partners/infinnity_solutions"><img src="/files/industry_partners/infinnity_logo.png"></a></td>
+						<td><h3>Infinnity Solutions</h3>
+						<p>Infinnity Solutions is a Russian company who develop innovative software solutions in the field of health. The main areas of software development are integrated electronic medical record, medical information systems and mobile medical applications. Since 2006, Infinnity Solutions is concerned with improving the efficiency of hospitals and health care systems as a whole.</p>
+						<a href="/industry_partners/infinnity_solutions">Learn more</a></td>
+					</tr>
 
-			<!-- Ocean -->
-			<tr>
-				<td><a href="/industry_partners/ocean_health_systems"><img src="/files/industry_partners/ocean_logo.png"></a></td>
-				<td><h2>Ocean Health Systems</h2>
-				<p>Ocean Health Systems is a recognised leader in e-health strategy, semantic interoperability and the shared EHR. Founded in 1998, Ocean Informatics is a unique company, comprising an experienced engineering team with strong clinical expertise. Collectively the Ocean team has over 100 person-years of health informatics experience. Ocean's vision is fully integrated and highly computable health data organised around the patient care process, served from high-availability infrastructure to point of care, reporting and analytics applications.</p>
-				<a href="/industry_partners/ocean_health_systems">Learn more</a></td>
-			</tr>
+				</tbody>
+				</table>
 
-			<!-- Tieto -->
-			<tr>
-				<td><a href="/industry_partners/tieto"><img src="/files/industry_partners/tieto_logo.png"></a></td>
-				<td><h2>Tieto</h2>
-				<p>Tieto is the leading Nordic software and services company whose goal is to be the customer's first choice for business renewal. In a rapidly changing world, every bit of information can be used to provide new value. Tieto aims to capture the significant opportunities of the data-driven world and turn them into lifelong value for people, business and society.</p>
-				<p>Healthcare is an important strategic area for Tieto.</p>
-				<a href="/industry_partners/tieto">Learn more</a></td>
-			</tr>
+			<h2>Micro org / startup Partner</h2>
+				<table class="TableInvisible">
+				<tbody>
+					<!-- Accenda -->
+					<tr>
+						<td><a href="/industry_partners/accenda"><img src="/files/industry_partners/accenda_logo.png"></a></td>
+						<td>
+							<h3>Accenda</h3>
+							<p>Accenda Limited is a UK-based software consulting business.  We help our clients streamline processes, reduce waste and improve efficiencies. We work closely with NHS Digital to help deliver solutions based on cutting-edge technology. We have a 'sleeves-rolled-up' approach to engaging on projects and are passionate about delivering solutions that deliver real value for patients, clinicians and stakeholders.</p>
+						<a href="/industry_partners/accenda">Learn more</a>
+						</td>
+					</tr>
 
-			<!-- Virtual Care -->
-			<tr>
-				<td><a href="/industry_partners/virtual_care"><img src="/files/industry_partners/virtual_care_logo.png"></a></td>
-				<td><h2>VirtualCare</h2>
-				<p>VirtualCare is a leading company in openEHR education and training. We provide online and in-company courses; we have also developed ebooks and tools, such as the openEHR modelling canvas kit.</p>
-				<a href="/industry_partners/virtual_care">Learn more</a></td>
-			</tr>
-			</tbody>
-			</table>
+					<!-- Inidus -->
+					<tr>
+						<td><a href="/industry_partners/inidus"><img src="/files/industry_partners/inidus_logo.png"></a></td>
+						<td><h3>Inidus</h3>
+						<p>inidus provides an open, secure, scalable health and care platform that enables developers to build health and care applications using open standards including HL7 FHIR, openEHR, SNOMED CT and IHE-XDS, making your data and applications portable and extensible.  The inidus platform enables rapid development allowing you to use any modern programming language, and our platform services to build integrated and scalable applications. Hosted in UK owned and located data centers meeting UK government and NHS standards for information governance and cyber security and offering N3 connectivity the inidus platform helps you meet your customer's requirements for connectivity and cyber security.</p>
+						<a href="/industry_partners/inidus">Learn more</a></td>
+					</tr>
+
+					<!-- IRYO -->
+					<tr>
+						<td><a href="/industry_partners/iryo"><img src="/files/industry_partners/iryo_logo.png"></a></td>
+						<td><h3>IRYO</h3>
+						<p>Iryo is a Slovenia-based IT company focused on improving the user experience of all stakeholders in the healthcare system. The Iryo system is using openEHR for increasing interoperability in healthcare, zero-knowledge data storage for data security and patient enforceable privacy. The company is building a global open-sourced permisionless protocol for healthcare solutions.</p>
+						<a href="/industry_partners/iryo">Learn more</a></td>
+					</tr>
+
+					<!-- MEDrecord 
+					<tr>
+						<td><a href="/industry_partners/medrecord"><img src="/files/industry_partners/medrecord_logo.png"></a></td>
+						<td><h3>MEDrecord BV</h3>
+						<p>MEDrecord provides a ground-breaking eHealth platform completely based on the openEHR standard. We have a long history in the eHealth landscape with significant expertise on security, continuous development and semantic interoperability. For developers and integrators we provide advanced, easy-to-use APIs for the development of clinically validated apps.</p>
+						<a href="/industry_partners/medrecord">Learn more</a></td>
+					</tr>
+					-->
+
+					<!-- Virtual Care -->
+					<tr>
+						<td><a href="/industry_partners/virtual_care"><img src="/files/industry_partners/virtual_care_logo.png"></a></td>
+						<td><h3>VirtualCare</h3>
+						<p>VirtualCare is a leading company in openEHR education and training. We provide online and in-company courses; we have also developed ebooks and tools, such as the openEHR modelling canvas kit.</p>
+						<a href="/industry_partners/virtual_care">Learn more</a></td>
+					</tr>
+				</tbody>
+				</table>
 		
 <!-- ------------------------------------------- Content ends here ------------------------------------------------- -->
 		</div>	
