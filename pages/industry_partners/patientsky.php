c5894853f195a37598887a50e78e7c6abac2c6cd
wolandscat <thomas.beale@openehr.org>
2020-02-11 11:01:06 +0000

    Upload recent graphics.

---
+++ pages/industry_partners/patientsky.php
@@ -0,0 +1,31 @@
+﻿<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_functions.php');
+
+//Change page name here:
+$PageName = 'PatientSky';
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header.php');
+?>
+
+		<div id="Content">
+<!-- - - - - - - - - - - - - - - - - - - - - - - Content starts here - - - - - - - - - - - - - - - - - - - - - - - - - -->
+			
+			<h1><?php echo "$PageName";?></h1>
+			
+			<a href="https://www.pasientsky.no" target="_blank"><img src="/files/industry_partners/patientsky_logo.png" class="IndustryLogo"></a>
+			
+			<p><em>Headquarters:</em>Norway</p>
+			<p><em>Size:</em> 6 employees</p>
+			<p><em>Trading since:</em> 2016</p>
+			<p><a href="https://www.pasientsky.no" target="_blank">website</a></p>
+			
+			<h2>Overview</h2>
+			<p>PasientSky is an innovative e-health player who wants more innovation and development in the healthcare sector.  With PasientSky, patients can take part in their own administration and release valuable resources for themselves and health professionals. PasientSky can make it easier for Healthcare professionals and patients to manage activities performed on a daily basis.  Their approach is to streamline and release resources in the complex and resourceful workflows that exist today.</p>
+
+			<h2>Solutions</h2>
+
+			<h2>Target Market</h2>
+
+<!-- - - - - - - - - - - - - - - - - - - - - - - Content ends here - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		</div>	
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_footer.php');?>
